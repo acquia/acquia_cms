8cdfeb50b772b3ca83bfeb8eb8158f84f7577567
Phéna Proxima <adam@phenaproxima.net>
2020-06-03 14:57:33 -0400

    Add a --generic option to the config:get Drush command to automatically strip out the UUID and site hash.

---
+++ modules/acquia_cms_common/src/Commands/Hooks.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace Drupal\acquia_cms_common\Commands;
+
+use Consolidation\AnnotatedCommand\CommandData;
+use Drush\Commands\DrushCommands;
+
+/**
+ * Implements Drush command hooks.
+ */
+final class Hooks extends DrushCommands {
+
+  /**
+   * Alters the result of the config:get command.
+   *
+   * @hook alter config:get
+   * @option $generic Automatically strip the UUID and site hash.
+   * @usage config:get --generic system.site
+   */
+  public function preConfigGet($result, CommandData $command_data) {
+    if ($command_data->input()->getOption('generic')) {
+      unset($result['uuid'], $result['_core']);
+    }
+    return $result;
+  }
+
+}
