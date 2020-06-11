5c4f168d262281f19852039f11356dd52dc1c99e
Adam <adam@phenaproxima.net>
2020-06-11 13:50:41 -0400

    ACMS-140: Integrate with Acquia Pipelines (#79)

---
+++ hooks/common/post-code-deploy/reinstall.sh
@@ -0,0 +1,14 @@
+#!/bin/sh
+#
+# Cloud Hook: Reinstall Acquia CMS
+#
+# Run `drush site-install acquia_cms` in the target environment.
+
+which drush
+drush --version
+
+site="$1"
+target_env="$2"
+
+# Fresh install of Acquia CMS.
+/usr/local/bin/drush9 @$site.$target_env site-install acquia_cms --account-pass=admin --yes
