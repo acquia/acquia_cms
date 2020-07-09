4e6e40e6cad2c585f55e17a84a00e494b9f58b1c
kunal-singh13 <67369064+kunal-singh13@users.noreply.github.com>
2020-07-09 13:24:02 +0530

    ACMS-53: As a content administrator I want tags and categories exposed on admin dashboards so I can easily filter my content (#108)
    * ACMS-53: Added filters in the content & media overview page
    
    * ACMS-53: Review comment changes

--- tests/src/ExistingSite/InstallStateTest.php
+++ tests/src/ExistingSite/InstallStateTest.php
@@ -3,6 +3,8 @@
 namespace Drupal\Tests\acquia_cms\ExistingSite;
 
 use Drupal\Core\Config\ImmutableConfig;
+use Drupal\taxonomy\Entity\Vocabulary;
+use Drupal\Tests\acquia_cms_common\Traits\MediaTestTrait;
 use weitzman\DrupalTestTraits\ExistingSiteBase;
 
 /**
@@ -11,6 +13,10 @@ use weitzman\DrupalTestTraits\ExistingSiteBase;
  * @group acquia_cms
  */
 class InstallStateTest extends ExistingSiteBase {
+
+  use MediaTestTrait {
+    createMedia as traitCreateMedia;
+  }
 
   /**
    * Assert that all install tasks have done what they should do.
@@ -39,6 +45,102 @@ class InstallStateTest extends ExistingSiteBase {
    */
   private function config(string $name) : ImmutableConfig {
     return $this->container->get('config.factory')->get($name);
+  }
+
+  /**
+   * Tests Categories and Tags filters in administrative content/media lists.
+   */
+  public function testAdminDashboardTagsCategories() {
+    $page = $this->getSession()->getPage();
+    $assert_session = $this->assertSession();
+
+    $account = $this->createUser();
+    $account->addRole('content_author');
+    $account->save();
+    $this->drupalLogin($account);
+
+    /** @var \Drupal\taxonomy\VocabularyInterface $categories_vocab */
+    $categories_vocab = Vocabulary::load('categories');
+    $category = $this->createTerm($categories_vocab, ['name' => 'Music']);
+
+    /** @var \Drupal\taxonomy\VocabularyInterface $tags_vocab */
+    $tags_vocab = Vocabulary::load('tags');
+    $tag = $this->createTerm($tags_vocab, ['name' => 'Rocks']);
+
+    // Create a node tagged with our new category, and another one tagged with
+    // our new tag, so we can test that the filters do what we expect.
+    $this->createNode([
+      'type' => 'page',
+      'title' => 'Categories Page',
+      'field_categories' => $category->id(),
+      'uid' => $account->id(),
+      'moderation_state' => 'published',
+    ]);
+    $this->createNode([
+      'type' => 'page',
+      'title' => 'Tags Page',
+      'field_tags' => $tag->id(),
+      'uid' => $account->id(),
+      'moderation_state' => 'published',
+    ]);
+
+    // Visit the content overview page.
+    $this->drupalGet('/admin/content');
+    $assert_session->statusCodeEquals(200);
+
+    // Try filtering by category.
+    $page->selectFieldOption('Categories', $category->label());
+    $page->pressButton('Filter');
+    $assert_session->linkExists('Categories Page');
+    $assert_session->linkNotExists('Tags Page');
+
+    // Now try filtering by tag.
+    $page->fillField('Tags', $tag->label());
+    $page->selectFieldOption('Categories', '');
+    $page->pressButton('Filter');
+    $assert_session->linkExists('Tags Page');
+    $assert_session->linkNotExists('Categories Page');
+
+    // Create a media item tagged with our new category, and another one tagged
+    // with our new tag, so we can test that the filters do what we expect.
+    $this->createMedia([
+      'uid' => $account->id(),
+      'bundle' => 'image',
+      'name' => 'Categories Media',
+      'field_categories' => $category->id(),
+    ]);
+    $this->createMedia([
+      'uid' => $account->id(),
+      'bundle' => 'image',
+      'name' => 'Tags Media',
+      'field_tags' => $tag->id(),
+    ]);
+
+    // Visit the media overview page.
+    $this->drupalGet('/admin/content/media');
+    $assert_session->statusCodeEquals(200);
+
+    // Try filtering by category.
+    $page->selectFieldOption('Categories', $category->label());
+    $page->pressButton('Filter');
+    $assert_session->linkExists('Categories Media');
+    $assert_session->linkNotExists('Tags Media');
+
+    // Try filtering by tag.
+    $page->fillField('Tags', $tag->label());
+    $page->selectFieldOption('Categories', '');
+    $page->pressButton('Filter');
+    $assert_session->linkExists('Tags Media');
+    $assert_session->linkNotExists('Categories Media');
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  private function createMedia(array $values = []) {
+    $media = $this->traitCreateMedia($values);
+    $this->markEntityForCleanup($media);
+    return $media;
   }
 
 }
