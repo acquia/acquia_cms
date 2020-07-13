af95cd1855570b519b833619086b36d7dc8467e7
Adam <adam@phenaproxima.net>
2020-07-13 13:50:08 -0400

    ACMS-172: Add a basic test of Cohesion integration (#117)

---
+++ tests/src/ExistingSiteJavascript/CohesionInstallTest.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace Drupal\Tests\acquia_cms\ExistingSiteJavascript;
+
+use Drupal\Tests\acquia_cms_common\ExistingSiteJavascript\CohesionTestBase;
+
+/**
+ * Tests that Cohesion is installed and operating correctly.
+ *
+ * @group acquia_cms
+ */
+class CohesionInstallTest extends CohesionTestBase {
+
+  /**
+   * Tests that Cohesion's layout canvas can be used.
+   */
+  public function testLayoutCanvas() {
+    $account = $this->createUser();
+    $account->addRole('administrator');
+    $account->save();
+    $this->drupalLogin($account);
+
+    $this->drupalGet('/node/add/page');
+    $canvas = $this->waitForElementVisible('css', '.coh-layout-canvas');
+
+    $component_added = $this->addComponent($canvas, 'Text');
+    $edit_form = $this->editComponent($component_added);
+    $edit_form->fillField('Component title', 'Example component');
+    $edit_form->pressButton('Apply');
+
+    $this->assertComponent($canvas, 'Example component');
+  }
+
+}
