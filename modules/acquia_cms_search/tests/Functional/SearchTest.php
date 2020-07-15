32864cb86c726bb72fd9343203a9d4462ec8625a
kunal-singh13 <67369064+kunal-singh13@users.noreply.github.com>
2020-07-15 16:52:10 +0530

    ACMS-58: Search API Integration (#115)
    * ACMS-58: Added the search functionality
    
    * ACMS-58: Review comment changes

---
+++ modules/acquia_cms_search/tests/Functional/SearchTest.php
@@ -0,0 +1,109 @@
+<?php
+
+namespace Drupal\Tests\acquia_cms_search\Functional;
+
+use Drupal\node\Entity\NodeType;
+use Drupal\Tests\BrowserTestBase;
+use Drupal\views\Entity\View;
+
+/**
+ * Tests the search functionality that ships with Acquia CMS.
+ *
+ * @group acquia_cms_search
+ * @group acquia_cms
+ */
+class SearchTest extends BrowserTestBase {
+
+  /**
+   * {@inheritdoc}
+   */
+  protected $defaultTheme = 'stark';
+
+  /**
+   * {@inheritdoc}
+   */
+  protected static $modules = [
+    'acquia_cms_search',
+    'acquia_cms_article',
+    'acquia_cms_event',
+    'acquia_cms_page',
+    'acquia_cms_person',
+    'acquia_cms_place',
+    'search_api_db',
+  ];
+
+  /**
+   * Disable strict config schema checks in this test.
+   *
+   * Cohesion has a lot of config schema errors, and until they are all fixed,
+   * this test cannot pass unless we disable strict config schema checking
+   * altogether. Since strict config schema isn't critically important in
+   * testing this functionality, it's okay to disable it for now, but it should
+   * be re-enabled (i.e., this property should be removed) as soon as possible.
+   *
+   * @var bool
+   */
+  // @codingStandardsIgnoreStart
+  protected $strictConfigSchema = FALSE;
+  // @codingStandardsIgnoreEnd
+
+  /**
+   * {@inheritdoc}
+   */
+  protected function setUp() {
+    parent::setUp();
+
+    /** @var \Drupal\views\ViewEntityInterface $view */
+    $view = View::load('search');
+    $display = &$view->getDisplay('default');
+    $display['display_options']['cache'] = [
+      'type' => 'none',
+      'options' => [],
+    ];
+    $view->save();
+  }
+
+  /**
+   * Tests the search functionality.
+   */
+  public function testSearch() {
+    $page = $this->getSession()->getPage();
+    $assert_session = $this->assertSession();
+
+    $account = $this->createUser();
+    $account->addRole('content_administrator');
+    $account->save();
+    $this->drupalLogin($account);
+
+    $node_types = NodeType::loadMultiple();
+    // Create some published and unpublished nodes to assert search
+    // functionality properly.
+    foreach ($node_types as $type) {
+      $published_node = $this->drupalCreateNode([
+        'type' => $type->id(),
+        'title' => 'Test published ' . $type->label(),
+        'moderation_state' => 'published',
+      ]);
+      $published_node->setPublished()->save();
+      $unpublished_node = $this->drupalCreateNode([
+        'type' => $type->id(),
+        'title' => 'Test unpublished ' . $type->label(),
+      ]);
+      $unpublished_node->setUnpublished()->save();
+    }
+
+    // Visit the seach page.
+    $this->drupalGet('/search');
+    $assert_session->statusCodeEquals(200);
+    $page->fillField('Keywords', 'Test');
+    $page->pressButton('Search');
+
+    foreach ($node_types as $type) {
+      // Check if only published nodes are visible.
+      $assert_session->linkExists('Test published ' . $type->label());
+      // Check if unpublished nodes are not visible.
+      $assert_session->linkNotExists('Test unpublished ' . $type->label());
+    }
+  }
+
+}
