0ec5ef964d6dc40f8742b911676a4275e5bc0ddc
Phéna Proxima <adam@phenaproxima.net>
2020-07-17 12:40:41 -0400

    ACMS-171: Support functional JS tests

--- acms-run-tests.sh
+++ acms-run-tests.sh
@@ -1,4 +1,6 @@
 #!/usr/bin/env bash
+
+# Automatically abort the script if any errors occur.
 set -e
 
 RED="\033[1;31m"
@@ -7,7 +9,7 @@ YELLOW="\033[1;33m"
 NOCOLOR="\033[0m"
 
 WEBSERVER_PORT=8080
-CHROMDRIVER_PORT=4444
+CHROMEDRIVER_PORT=4444
 
 echo -e "${GREEN}Running on ${OSTYPE}${NOCOLOR}"
 
@@ -20,11 +22,11 @@ runwebserver() {
   echo -e "${GREEN}Drush server started on port "${WEBSERVER_PORT}".${NOCOLOR}"
 }
 
-# Run chromedriver on port "${CHROMDRIVER_PORT}" (assuming mink driver port is set to "${CHROMDRIVER_PORT}" in phpunit.xml).
+# Run ChromeDriver on port "${CHROMEDRIVER_PORT}".
 runchromedriver() {
-  echo -e "${YELLOW}Starting Chromedriver on port "${CHROMDRIVER_PORT}".${NOCOLOR}"
-  nohup chromedriver --port="${CHROMDRIVER_PORT}" &
-  echo -e "${GREEN}Started Chromedriver on port "${CHROMDRIVER_PORT}".${NOCOLOR}"
+  echo -e "${YELLOW}Starting ChromeDriver on port "${CHROMEDRIVER_PORT}".${NOCOLOR}"
+  nohup chromedriver --port="${CHROMEDRIVER_PORT}" &
+  echo -e "${GREEN}Started ChromeDriver on port "${CHROMEDRIVER_PORT}".${NOCOLOR}"
 }
 
 # Kill any process on a linux GNU environment.
@@ -45,18 +47,18 @@ killProcessDarwinOs() {
   echo -e "${YELLOW}Process killed on port $1 ${NOCOLOR}"
 }
 
-# Switch case to handle mac os and linux.
+# Switch case to handle macOS and Linux.
 case $OSTYPE in
   "linux-gnu"*)
     if declare -a array=($(tail -n +2 /proc/net/tcp | cut -d":" -f"3"|cut -d" " -f"1")) &&
       for port in ${array[@]}; do echo $((0x$port)); done | grep "${WEBSERVER_PORT}" ; then
-        echo -e "${RED}Port "${WEBSERVER_PORT}" is already occupied. Webserver cannot start on port "${WEBSERVER_PORT}".${NOCOLOR}"
+        echo -e "${RED}Port "${WEBSERVER_PORT}" is already occupied. Web server cannot start on port "${WEBSERVER_PORT}".${NOCOLOR}"
       else
         runwebserver
     fi
     if declare -a array=($(tail -n +2 /proc/net/tcp | cut -d":" -f"3"|cut -d" " -f"1")) &&
-      for port in ${array[@]}; do echo $((0x$port)); done | grep "${CHROMDRIVER_PORT}" ; then
-        echo -e "${RED}Port "${CHROMDRIVER_PORT}" is already occupied. Chromedriver cannot run on port "${CHROMDRIVER_PORT}". ${NOCOLOR}"
+      for port in ${array[@]}; do echo $((0x$port)); done | grep "${CHROMEDRIVER_PORT}" ; then
+        echo -e "${RED}Port "${CHROMEDRIVER_PORT}" is already occupied. ChromeDriver cannot run on port "${CHROMEDRIVER_PORT}". ${NOCOLOR}"
       else
         runchromedriver
     fi
@@ -65,12 +67,12 @@ case $OSTYPE in
       if [ -z "$(lsof -t -i:"${WEBSERVER_PORT}")" ] ; then
         runwebserver
       else
-        echo -e "${RED}Port "${WEBSERVER_PORT}" is already occupied. Webserver cannot start on port "${WEBSERVER_PORT}". ${NOCOLOR}"
+        echo -e "${RED}Port "${WEBSERVER_PORT}" is already occupied. Web server cannot start on port "${WEBSERVER_PORT}". ${NOCOLOR}"
       fi
-      if [ -z "$(lsof -t -i:"${CHROMDRIVER_PORT}")" ] ; then
+      if [ -z "$(lsof -t -i:"${CHROMEDRIVER_PORT}")" ] ; then
         runchromedriver
       else
-        echo -e "${RED}Port "${CHROMDRIVER_PORT}" is already occupied. Chromedriver cannot run on port "${CHROMDRIVER_PORT}". ${NOCOLOR}"
+        echo -e "${RED}Port "${CHROMEDRIVER_PORT}" is already occupied. ChromeDriver cannot run on port "${CHROMEDRIVER_PORT}". ${NOCOLOR}"
       fi
       ;;
 esac
@@ -83,7 +85,7 @@ if [ -z "$(printenv SIMPLETEST_DB)" ] ; then
   export SIMPLETEST_DB=sqlite://localhost/drupal.sqlite
   echo -e "${GREEN}SIMPLETEST_DB environment variable is now set as: ${NOCOLOR}"
   printenv SIMPLETEST_DB
-  echo -e "${YELLOW}If you are using SQL, set environment variable accordingly, ex (mysql://drupal:drupal@127.0.0.1/drupal)${NOCOLOR}"
+  echo -e "${YELLOW}If you are using MySQL or PostgreSQL, set the environment variable accordingly, e.g., mysql://drupal:drupal@127.0.0.1/drupal${NOCOLOR}"
 fi
 
 # Set SIMPLETEST_BASE_URL environment variable if it is not set already.
@@ -91,6 +93,27 @@ if [ -z "$(printenv SIMPLETEST_BASE_URL)" ] ; then
   export SIMPLETEST_BASE_URL=http://127.0.0.1:"${WEBSERVER_PORT}"
   echo -e "${GREEN}SIMPLETEST_BASE_URL environment variable is now set as: ${NOCOLOR}"
   printenv SIMPLETEST_BASE_URL
+fi
+
+# Set DTT_BASE_URL environment variable if it is not set already.
+if [ -z "$(printenv DTT_BASE_URL)" ] ; then
+  export DTT_BASE_URL=$SIMPLETEST_BASE_URL
+  echo -e "${GREEN}DTT_BASE_URL environment variable is now set as: ${NOCOLOR}"
+  printenv DTT_BASE_URL
+fi
+
+# Set MINK_DRIVER_ARGS_WEBDRIVER environment variable if it is not set already.
+if [ -z "$(printenv MINK_DRIVER_ARGS_WEBDRIVER)" ] ; then
+  export MINK_DRIVER_ARGS_WEBDRIVER='["chrome", {"chrome": {"switches": ["headless"]}}, "http://127.0.0.1:4444"]'
+  echo -e "${GREEN}MINK_DRIVER_ARGS_WEBDRIVER environment variable is now set as: ${NOCOLOR}"
+  printenv MINK_DRIVER_ARGS_WEBDRIVER
+fi
+
+# Set DTT_MINK_DRIVER_ARGS environment variable if it is not set already.
+if [ -z "$(printenv DTT_MINK_DRIVER_ARGS)" ] ; then
+  export DTT_MINK_DRIVER_ARGS=$MINK_DRIVER_ARGS_WEBDRIVER
+  echo -e "${GREEN}DTT_MINK_DRIVER_ARGS environment variable is now set as: ${NOCOLOR}"
+  printenv DTT_MINK_DRIVER_ARGS
 fi
 
 # Set SYMFONY_DEPRECATIONS_HELPER environment variable if it is not set already.
@@ -103,20 +126,20 @@ fi
 # Run all automated PHPUnit tests.
 # If --stop-on-failure is passed as an argument $1 will handle it.
 echo -e "${YELLOW}Running phpunit tests for acquia_cms. ${NOCOLOR}"
-COMPOSER_PROCESS_TIMEOUT=0 ./vendor/bin/phpunit -c docroot/core docroot/sites/all/modules/ --testsuite=functional --debug -v $1
+COMPOSER_PROCESS_TIMEOUT=0 ./vendor/bin/phpunit -c docroot/core docroot/profiles/acquia_cms --debug $1
 
-# Stop Chrome driver and drush server based on OS Type.
+# Stop ChromeDriver and Drush web server based on OS Type.
 case $OSTYPE in
    "linux-gnu"*)
     echo -e "${YELLOW}Stopping drush webserver.${NOCOLOR}"
     killProcessLinuxOs "${WEBSERVER_PORT}"
     echo -e "${YELLOW}Stopping chromedriver.${NOCOLOR}"
-    killProcessLinuxOs "${CHROMDRIVER_PORT}"
+    killProcessLinuxOs "${CHROMEDRIVER_PORT}"
     ;;
    "darwin"*)
     echo -e "${YELLOW}Stopping drush webserver.${NOCOLOR}"
     killProcessDarwinOs "${WEBSERVER_PORT}"
     echo -e "${YELLOW}Stopping chromedriver.${NOCOLOR}"
-    killProcessDarwinOs "${CHROMDRIVER_PORT}"
+    killProcessDarwinOs "${CHROMEDRIVER_PORT}"
     ;;
 esac
