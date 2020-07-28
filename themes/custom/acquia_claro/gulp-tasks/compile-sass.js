ea36782f33c92c9536b8c6217ab88558196badf6
Ganesh <ganeshdevkate@gmail.com>
2020-07-28 10:16:49 -0400

    ACMS-90: basic setup for acquia claro theme.

---
+++ themes/custom/acquia_claro/gulp-tasks/compile-sass.js
@@ -0,0 +1,29 @@
+/**
+ * @file
+ * Task: Compile: Sass.
+ */
+
+/* global module */
+
+module.exports = function (gulp, plugins, options) {
+  'use strict';
+
+  gulp.task('compile:sass', gulp.series(async function compile () {
+    return gulp.src([
+      options.sass.files
+    ])
+      .pipe(plugins.plumber())
+      .pipe(plugins.sourcemaps.init())
+      .pipe(plugins.sassGlob())
+      .pipe(plugins.sass({
+        errLogToConsole: true,
+        outputStyle: 'expanded'
+      }))
+      .pipe(plugins.autoprefixer({
+        browsers: ['last 2 versions'],
+        cascade: false
+      }))
+      .pipe(plugins.sourcemaps.write())
+      .pipe(gulp.dest(options.sass.destination));
+  }));
+};
