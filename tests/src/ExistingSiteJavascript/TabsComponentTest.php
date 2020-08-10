e1819ac645e4ac2ad60cc7c9d64a42c6dd2ba4b3
Chandan Singh <63041696+chandan-singh7929@users.noreply.github.com>
2020-08-10 17:08:59 +0530

    ACMS-223: Repurpose tab container & item components (#170)
    * ACMS-223: Repurpose tab container & item componets
    
    * ACMS-223: Remove componets from uikit
    
    * ACMS-223: test coverage for tabs container & tab item
    
    * ACMS-223: Refactor on top of changes from ACMS-225
    
    * ACMS-223: Fixed dark theme color scheme
    
    Co-authored-by: Phéna Proxima <adam@phenaproxima.net>

---
+++ tests/src/ExistingSiteJavascript/TabsComponentTest.php
@@ -0,0 +1,64 @@
+<?php
+
+namespace Drupal\Tests\acquia_cms\ExistingSiteJavascript;
+
+/**
+ * Tests the "Tab container horizontal" and "Tab item" components.
+ *
+ * @group acquia_cms
+ */
+class TabsComponentTest extends CohesionTestBase {
+
+  /**
+   * Test that the components can be added to a layout canvas.
+   */
+  public function testComponent() {
+    $account = $this->createUser();
+    $account->addRole('administrator');
+    $account->save();
+    $this->drupalLogin($account);
+
+    $this->drupalGet('/node/add/page');
+
+    // Add the component to the layout canvas.
+    $canvas = $this->waitForElementVisible('css', '.coh-layout-canvas');
+    $tabs_container = $this->addComponent($canvas, 'Tabs container - horizontal tabs');
+    $tab_item = $this->addComponentToDropZone($tabs_container, 'Tab item');
+    $this->addComponentToDropZone($tab_item, 'Text and image');
+  }
+
+  /**
+   * Tests that the components can be edited by a specific user role.
+   *
+   * @param string $role
+   *   The ID of the user role to test with.
+   *
+   * @dataProvider providerEditAccess
+   */
+  public function testEditAccess(string $role) {
+    $account = $this->createUser();
+    $account->addRole($role);
+    $account->save();
+    $this->drupalLogin($account);
+
+    // Visit to cohesion components page.
+    $this->drupalGet('/admin/cohesion/components/components');
+    $this->editComponentDefinition('Interactive components', 'Tabs container - horizontal tabs');
+    $this->getSession()->back();
+    $this->editComponentDefinition('Interactive components', 'Tab item');
+  }
+
+  /**
+   * Data provider for ::testEditAccess().
+   *
+   * @return array[]
+   *   Sets of arguments to pass to the test method.
+   */
+  public function providerEditAccess() {
+    return [
+      ['site_builder'],
+      ['developer'],
+    ];
+  }
+
+}
