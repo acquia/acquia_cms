f4660ee8f091ba9361b90959c7d43a83480073a3
kunal-singh13 <67369064+kunal-singh13@users.noreply.github.com>
2020-08-11 20:12:13 +0530

    ACMS-183: Add listing page for Place content (#178)

---
+++ modules/acquia_cms_place/tests/src/ExistingSite/PlaceListTest.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace Drupal\Tests\acquia_cms_place\ExistingSite;
+
+use Drupal\Tests\acquia_cms_common\ExistingSite\ContentTypeListTestBase;
+use Drupal\views\Entity\View;
+
+/**
+ * Tests the "all places" listing page.
+ *
+ * @group acquia_cms
+ * @group acquia_cms_place
+ */
+class PlaceListTest extends ContentTypeListTestBase {
+
+  /**
+   * {@inheritdoc}
+   */
+  protected $nodeType = 'place';
+
+  /**
+   * {@inheritdoc}
+   */
+  protected function getView() : View {
+    return View::load('places');
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  protected function visitListPage() : void {
+    $this->drupalGet('/places');
+  }
+
+}
