d04fdc2d5939b176836d8c59bbce39a81ea3a58d
Chandan Singh <63041696+chandan-singh7929@users.noreply.github.com>
2020-08-11 20:10:49 +0530

    ACMS-181: Create listing page for Person content (#180)

---
+++ modules/acquia_cms_person/tests/src/ExistingSite/PersonListTest.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace Drupal\Tests\acquia_cms_person\ExistingSite;
+
+use Drupal\Tests\acquia_cms_common\ExistingSite\ContentTypeListTestBase;
+use Drupal\views\Entity\View;
+
+/**
+ * Tests the "all people" listing page.
+ *
+ * @group acquia_cms_person
+ * @group acquia_cms
+ */
+class PersonListTest extends ContentTypeListTestBase {
+
+  /**
+   * {@inheritdoc}
+   */
+  protected $nodeType = 'person';
+
+  /**
+   * {@inheritdoc}
+   */
+  protected function getView() : View {
+    return View::load('people');
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  protected function visitListPage() : void {
+    $this->drupalGet('/people');
+  }
+
+}
