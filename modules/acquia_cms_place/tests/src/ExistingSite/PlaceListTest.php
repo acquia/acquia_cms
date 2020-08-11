ea4ccbf676c23eae16b3143c76a7e15944e1a52e
Phéna Proxima <adam@phenaproxima.net>
2020-08-11 17:23:29 -0400

    ACMS-181: Fix places view

--- modules/acquia_cms_place/tests/src/ExistingSite/PlaceListTest.php
+++ modules/acquia_cms_place/tests/src/ExistingSite/PlaceListTest.php
@@ -2,6 +2,7 @@
 
 namespace Drupal\Tests\acquia_cms_place\ExistingSite;
 
+use Drupal\Core\Entity\Query\QueryInterface;
 use Drupal\Tests\acquia_cms_common\ExistingSite\ContentTypeListTestBase;
 use Drupal\views\Entity\View;
 
@@ -30,6 +31,13 @@ class PlaceListTest extends ContentTypeListTestBase {
    */
   protected function visitListPage() : void {
     $this->drupalGet('/places');
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  protected function getQuery() : QueryInterface {
+    return parent::getQuery()->sort('title', 'ASC');
   }
 
 }
