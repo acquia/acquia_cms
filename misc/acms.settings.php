27dd73bbc112936b922fd8af75211ad9892ad3bf
Michael Sherron <msherron@gmail.com>
2020-08-21 16:44:54 -0700

    ACMS-309: fixing parens error and patching memcache.

--- misc/acms.settings.php
+++ misc/acms.settings.php
@@ -33,7 +33,7 @@ if (AcquiaDrupalEnvironmentDetector::isAhEnv()) {
 // Use memcache as cache backend if Acquia configuration is present.
 $repo_root = dirname(DRUPAL_ROOT);
 $memcacheSettingsFile = $repo_root . '/vendor/acquia/memcache-settings/memcache.settings.php';
-if (file_exists($memcacheSettingsFile && getenv('ENABLE_MEMCACHED'))) {
+if (file_exists($memcacheSettingsFile) && getenv('ENABLE_MEMCACHED')) {
   // phpcs:ignore
   require $memcacheSettingsFile;
 }
