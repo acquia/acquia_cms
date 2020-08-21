2e46c30275aeaceddb05b2f9ec4d8d4bb7a671b1
msherron <msherron@gmail.com>
2020-08-21 13:51:27 -0700

    ACMS-309: Adding patch for drupal-environment-detector.

--- misc/acms.settings.php
+++ misc/acms.settings.php
@@ -14,11 +14,18 @@ use Acquia\DrupalEnvironmentDetector\AcquiaDrupalEnvironmentDetector;
  * This is always set and exposed by the Drupal Kernel.
  */
 // phpcs:ignore
+$site_name = AcquiaDrupalEnvironmentDetector::getSiteName($site_path);
+// Acquia Cloud settings.
 if (AcquiaDrupalEnvironmentDetector::isAhEnv()) {
-  if (AcquiaDrupalEnvironmentDetector::isAhEnv()) {
-    $ah_group = AcquiaDrupalEnvironmentDetector::getAhGroup();
-    if (!AcquiaDrupalEnvironmentDetector::isAcsfEnv()) {
+  $ah_group = AcquiaDrupalEnvironmentDetector::getAhGroup();
+  if (!AcquiaDrupalEnvironmentDetector::isAcsfEnv()) {
+    if ($site_name == 'default') {
       require "/var/www/site-php/$ah_group/$ah_group-settings.inc";
+    }
+    else {
+      // Acquia Cloud does not support periods in db names.
+      $safe_site_name = str_replace('.', '_', $site_name);
+      require "/var/www/site-php/$ah_group/$safe_site_name-settings.inc";
     }
   }
 }
