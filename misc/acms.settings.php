c743af6af7c0a3fb62ee92e15d676d98dbf0f883
Michael Sherron <michael.sherron@acquia.com>
2020-08-20 23:35:57 -0400

    ACMS-309: Fixing path and phpcs errors.

--- misc/acms.settings.php
+++ misc/acms.settings.php
@@ -1,28 +1,32 @@
 <?php
 
+/**
+ * @file
+ * Auto-connect to the correct Acquia credentials file.
+ */
+
 use Acquia\DrupalEnvironmentDetector\AcquiaDrupalEnvironmentDetector;
 
+/**
+ * Site path.
+ *
+ * @var $site_path
+ * This is always set and exposed by the Drupal Kernel.
+ */
+// phpcs:ignore
 if (AcquiaDrupalEnvironmentDetector::isAhEnv()) {
-  if ( AcquiaDrupalEnvironmentDetector::isAhEnv()) {
+  if (AcquiaDrupalEnvironmentDetector::isAhEnv()) {
     $ah_group = AcquiaDrupalEnvironmentDetector::getAhGroup();
-    if (! AcquiaDrupalEnvironmentDetector::isAcsfEnv()) {
-      if ($site_name == 'default') {
-        $settings_files[] = "/var/www/site-php/$ah_group/$ah_group-settings.inc";
-      } else {
-        // Acquia Cloud does not support periods in db names.
-        $safe_site_name = str_replace('.', '_', $site_name);
-        $settings_files[] = "/var/www/site-php/$ah_group/$safe_site_name-settings.inc";
-      }
+    if (!AcquiaDrupalEnvironmentDetector::isAcsfEnv()) {
+      require "/var/www/site-php/$ah_group/$ah_group-settings.inc";
     }
   }
+}
 
-  /**
-   * Use memcache as cache backend if Acquia configuration is present.
-   */
-  $repo_root = dirname(DRUPAL_ROOT);
-  $memcacheSettingsFile = $repo_root . '/vendor/acquia/memcache-settings/memcache.settings.php';
-  if (file_exists($memcacheSettingsFile && getenv('ENABLE_MEMCACHED'))) {
-    // phpcs:ignore
-    require $memcacheSettingsFile;
-  }
+// Use memcache as cache backend if Acquia configuration is present.
+$repo_root = dirname(DRUPAL_ROOT);
+$memcacheSettingsFile = $repo_root . '/vendor/acquia/memcache-settings/memcache.settings.php';
+if (file_exists($memcacheSettingsFile && getenv('ENABLE_MEMCACHED'))) {
+  // phpcs:ignore
+  require $memcacheSettingsFile;
 }
