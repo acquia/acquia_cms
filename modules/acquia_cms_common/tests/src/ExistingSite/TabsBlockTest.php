021409608d2e71e649e2fd1b32c27bb72ac4f8b2
Phéna Proxima <adam@phenaproxima.net>
2020-08-24 15:23:57 -0400

    ACMS-296: Cleanup TabsBlockTest

--- modules/acquia_cms_common/tests/src/ExistingSite/TabsBlockTest.php
+++ modules/acquia_cms_common/tests/src/ExistingSite/TabsBlockTest.php
@@ -5,7 +5,7 @@ namespace Drupal\Tests\acquia_cms_common\ExistingSite;
 use weitzman\DrupalTestTraits\ExistingSiteBase;
 
 /**
- * Test Drupal Core tabs block appears for user roles.
+ * Tests that the Drupal core tabs block behaves as expected.
  *
  * @group acquia_cms_common
  * @group acquia_cms
@@ -13,99 +13,43 @@ use weitzman\DrupalTestTraits\ExistingSiteBase;
 class TabsBlockTest extends ExistingSiteBase {
 
   /**
-   * Test node.
-   *
-   * @var \Drupal\node\NodeInterface
-   */
-  protected $node1;
-
-  /**
-   * Test node.
-   *
-   * @var \Drupal\node\NodeInterface
-   */
-  protected $node2;
-
-  /**
-   * Test node.
-   *
-   * @var \Drupal\node\NodeInterface
-   */
-  protected $node3;
-
-  /**
-   * Test node.
-   *
-   * @var \Drupal\node\NodeInterface
-   */
-  protected $node4;
-
-  /**
-   * Test node.
-   *
-   * @var \Drupal\node\NodeInterface
-   */
-  protected $node5;
-
-  /**
-   * Test that on node view page tabs block is appearing.
+   * Tests that the tabs block appears on node pages.
    *
    * @param string $role
    *   The ID of the user role to test with.
    *
-   * @dataProvider providerViewAccess
+   * @dataProvider providerTabsBlock
    */
-  public function testTabsBlockAppears(string $role) {
+  public function testTabsBlock(string $role) {
     $account = $this->createUser();
     $account->addRole($role);
     $account->save();
     $this->drupalLogin($account);
 
-    $this->node1 = $this->createNode([
-      'type' => 'article',
-      'title' => 'Article Page',
-      'uid' => $account->id(),
-    ]);
-    $this->node2 = $this->createNode([
-      'type' => 'event',
-      'title' => 'Event Page',
-      'uid' => $account->id(),
-    ]);
-    $this->node3 = $this->createNode([
-      'type' => 'page',
-      'title' => 'Basic Page',
-      'uid' => $account->id(),
-    ]);
-    $this->node4 = $this->createNode([
-      'type' => 'person',
-      'title' => 'Person Page',
-      'uid' => $account->id(),
-    ]);
-    $this->node5 = $this->createNode([
-      'type' => 'place',
-      'title' => 'Place Page',
-      'uid' => $account->id(),
-    ]);
+    $assert_session = $this->assertSession();
 
-    $this->drupalGet("/node/{$this->node1->id()}");
-    $this->assertSession()->elementExists('css', '#block-tabs');
-    $this->drupalGet("/node/{$this->node2->id()}");
-    $this->assertSession()->elementExists('css', '#block-tabs');
-    $this->drupalGet("/node/{$this->node3->id()}");
-    $this->assertSession()->elementExists('css', '#block-tabs');
-    $this->drupalGet("/node/{$this->node4->id()}");
-    $this->assertSession()->elementExists('css', '#block-tabs');
-    $this->drupalGet("/node/{$this->node5->id()}");
-    $this->assertSession()->elementExists('css', '#block-tabs');
+    $node_types = $this->container->get('entity_type.manager')
+      ->getStorage('node_type')
+      ->getQuery()
+      ->execute();
+
+    foreach ($node_types as $node_type) {
+      $node = $this->createNode([
+        'type' => $node_type,
+      ]);
+      $this->assertSame($account->id(), $node->getOwnerId());
+      $this->drupalGet($node->toUrl());
+      $assert_session->elementExists('css', '#block-tabs');
+    }
   }
 
   /**
-   * Data provider for testing tabs block on node view page.
+   * Data provider for ::testTabsBlock().
    *
    * @return array[]
    *   Sets of arguments to pass to the test method.
    */
-  public function providerViewAccess() {
+  public function providerTabsBlock() {
     return [
       ['administrator'],
       ['content_administrator'],
