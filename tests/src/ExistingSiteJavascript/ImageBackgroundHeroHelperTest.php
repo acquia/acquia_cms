a2db0540523ce6e86ce2c5416b4967f132677c96
dishabhadra <disha.bhadra@acquia.com>
2020-08-31 19:50:13 +0530

    ACMS-267: created the image background hero helper and test coverage.

---
+++ tests/src/ExistingSiteJavascript/ImageBackgroundHeroHelperTest.php
@@ -0,0 +1,51 @@
+<?php
+
+namespace Drupal\Tests\acquia_cms\ExistingSiteJavascript;
+
+/**
+ * Tests the "Image Background Hero" helper.
+ *
+ * @group acquia_cms
+ */
+class ImageBackgroundHeroHelperTest extends CohesionTestBase {
+
+  /**
+   * Tests that the helper can be added to a layout canvas.
+   *
+   * @param string[] $roles
+   *   Additional user roles to apply to the account being logged in.
+   *
+   * @dataProvider providerHelperInstallation
+   */
+  public function testHelper(array $roles = []) {
+    $account = $this->createUser();
+    array_walk($roles, [$account, 'addRole']);
+    $account->save();
+    $this->drupalLogin($account);
+
+    $this->drupalGet('/node/add/page');
+
+    // Add the helper to the layout canvas.
+    $canvas = $this->waitForElementVisible('css', '.coh-layout-canvas');
+    $this->addHelper($canvas, 'Image Background Hero', 'Background image container');
+  }
+
+  /**
+   * Tests that helper can be edited by a specific user role.
+   *
+   * @param string $role
+   *   The ID of the user role to test with.
+   *
+   * @dataProvider providerEditAccess
+   */
+  public function testEditAccess(string $role) {
+    $account = $this->createUser();
+    $account->addRole($role);
+    $account->save();
+    $this->drupalLogin($account);
+
+    $this->drupalGet('/admin/cohesion/helpers/helpers');
+    $this->editHelperDefinition('Layout helpers', 'Image Background Hero');
+  }
+
+}
