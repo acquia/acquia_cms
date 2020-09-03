ee73f6e1b125346a6fc922b5a6d192d88e8ef68e
Kunal Singh <kunal.singh@acquia.com>
2020-09-04 02:17:32 +0530

    ACMS-68: Review comment changes

--- modules/acquia_cms_json_api/src/EventSubscriber/RestrictResourceEventSubscriber.php
+++ modules/acquia_cms_json_api/src/EventSubscriber/RestrictResourceEventSubscriber.php
@@ -2,14 +2,37 @@
 
 namespace Drupal\acquia_cms_json_api\EventSubscriber;
 
+use Drupal\Core\Entity\EntityTypeManagerInterface;
 use Drupal\jsonapi\ResourceType\ResourceTypeBuildEvent;
 use Drupal\jsonapi\ResourceType\ResourceTypeBuildEvents;
 use Symfony\Component\EventDispatcher\EventSubscriberInterface;
 
 /**
- * Event Subscriber to restrict access to some resource type.
+ * Event Subscriber to restrict access to some resource types.
+ *
+ * @internal
+ *   This is a totally internal part of Acquia CMS and may be changed in any
+ *   way, or removed outright, at any time without warning. External code should
+ *   not use this class!
  */
-class RestrictResourceEventSubscriber implements EventSubscriberInterface {
+final class RestrictResourceEventSubscriber implements EventSubscriberInterface {
+
+  /**
+   * The entity type manager.
+   *
+   * @var \Drupal\Core\Entity\EntityTypeManagerInterface
+   */
+  protected $entityTypeManager;
+
+  /**
+   * Instantiates a RestrictResourceEventSubscriber object.
+   *
+   * @param \Drupal\Core\Entity\EntityTypeManagerInterface $entity_type_manager
+   *   The entity type manager.
+   */
+  public function __construct(EntityTypeManagerInterface $entity_type_manager) {
+    $this->entityTypeManager = $entity_type_manager;
+  }
 
   /**
    * {@inheritdoc}
@@ -30,16 +53,8 @@ class RestrictResourceEventSubscriber implements EventSubscriberInterface {
    *   The build event.
    */
   public function disableResourceType(ResourceTypeBuildEvent $event) {
-    $resource_type_name = $event->getResourceTypeName();
-    if (!(strpos($resource_type_name, 'taxonomy_term--') === 0 ||
-        strpos($resource_type_name, 'node--') === 0 ||
-        strpos($resource_type_name, 'media--') === 0)) {
+    if (!$this->allowedResourceType($event->getResourceTypeName())) {
       $event->disableResourceType();
-    }
-    foreach ($event->getFields() as $field) {
-      if (in_array($field->getInternalName(), ["nid", "mid", "tid"])) {
-        $event->disableField($field);
-      }
     }
   }
 
@@ -50,16 +65,34 @@ class RestrictResourceEventSubscriber implements EventSubscriberInterface {
    *   The build event.
    */
   public function disableResourceTypeFields(ResourceTypeBuildEvent $event) {
-    $resource_type_name = $event->getResourceTypeName();
-    if (strpos($resource_type_name, 'taxonomy_term--') === 0 ||
-        strpos($resource_type_name, 'node--') === 0 ||
-        strpos($resource_type_name, 'media--') === 0) {
+    if ($this->allowedResourceType($event->getResourceTypeName())) {
+      $ids = [
+        $this->entityTypeManager->getDefinition('node')->getKey('id'),
+        $this->entityTypeManager->getDefinition('media')->getKey('id'),
+        $this->entityTypeManager->getDefinition('taxonomy_term')->getKey('id'),
+      ];
       foreach ($event->getFields() as $field) {
-        if (in_array($field->getInternalName(), ["nid", "mid", "tid"])) {
+        if (in_array($field->getInternalName(), $ids)) {
           $event->disableField($field);
         }
       }
     }
+  }
+
+  /**
+   * Checks if the given resource type name is from the allowed resources.
+   *
+   * @param string $resource_type_name
+   *   The name of the resource type.
+   */
+  protected function allowedResourceType(string $resource_type_name) : bool {
+    if (strpos($resource_type_name, 'taxonomy_term--') === 0 ||
+        strpos($resource_type_name, 'node--') === 0 ||
+        strpos($resource_type_name, 'media--') === 0) {
+      return TRUE;
+    }
+
+    return FALSE;
   }
 
 }
--- modules/acquia_cms_json_api/tests/src/Functional/JsonApiTest.php
+++ modules/acquia_cms_json_api/tests/src/Functional/JsonApiTest.php
@@ -3,13 +3,14 @@
 namespace Drupal\Tests\acquia_cms_json_api\Functional;
 
 use Drupal\Component\Serialization\Json;
+use Drupal\Core\Entity\EntityInterface;
 use Drupal\Core\Url;
 use Drupal\field\Entity\FieldConfig;
 use Drupal\field\Entity\FieldStorageConfig;
+use Drupal\media\Entity\Media;
 use Drupal\taxonomy\Entity\Vocabulary;
-use Drupal\Tests\acquia_cms_common\Traits\MediaTestTrait;
-use Drupal\Tests\BrowserTestBase;
-use Drupal\Tests\jsonapi\Functional\JsonApiRequestTestTrait;
+use Drupal\Tests\jsonapi\Functional\JsonApiFunctionalTestBase;
+use Drupal\Tests\media\Traits\MediaTypeCreationTrait;
 use Drupal\Tests\taxonomy\Traits\TaxonomyTestTrait;
 
 /**
@@ -18,11 +19,10 @@ use Drupal\Tests\taxonomy\Traits\TaxonomyTestTrait;
  * @group acquia_cms_json_api
  * @group acquia_cms
  */
-class JsonApiTest extends BrowserTestBase {
+class JsonApiTest extends JsonApiFunctionalTestBase {
 
-  use MediaTestTrait;
   use TaxonomyTestTrait;
-  use JsonApiRequestTestTrait;
+  use MediaTypeCreationTrait;
 
   /**
    * Disable strict config schema checks in this test.
@@ -42,56 +42,80 @@ class JsonApiTest extends BrowserTestBase {
   /**
    * {@inheritdoc}
    */
-  protected $defaultTheme = 'stark';
+  protected static $modules = [
+    'media',
+    'acquia_cms_json_api',
+  ];
 
   /**
    * {@inheritdoc}
    */
-  protected static $modules = [
-    'acquia_cms_article',
-    'acquia_cms_image',
-    'acquia_cms_json_api',
-  ];
+  protected function setUp() {
+    parent::setUp();
+    // Create media type for test.
+    $this->createMediaType('image', [
+      'id' => 'image',
+      'label' => 'Image',
+    ]);
+    // Flusing cache to make drupal aware of the new mediatype else the media
+    // endpoint will through 404.
+    drupal_flush_all_caches();
+  }
 
   /**
    * Assert that all the endpoints are working as expected.
    *
    * - node, taxonomy_term, media, and taxonomy_vocabulary are accessible.
    * - Rest all resource type should not be accessible.
+   *
+   * @param string[]|null $roles
+   *   The user role(s) to test with, or NULL to test as an anonymous user. If
+   *   this is an empty array, the test will run as an authenticated user with
+   *   no additional roles.
+   *
+   * @dataProvider providerNonRestrictedEndpoints
    */
-  public function testNonRestrictedEndpoints() {
+  public function testNonRestrictedEndpoints(?array $roles) {
     // 1. GET, POST, PATCH request to node Resource type.
     $node = $this->createNode(['type' => 'article', 'moderation_state' => 'published']);
-    $this->assertEndpoint($node, 'nid', 'node');
+    $this->assertEndpoint($node, 'nid', 'node', $roles);
 
     // 2. GET, POST, PATCH request to media Resource type.
-    $media = $this->createMedia(['bundle' => 'image']);
-    $this->assertEndpoint($media, 'mid', 'media');
+    $media = Media::create([
+      'bundle' => 'image',
+      'name' => 'Test Image',
+    ]);
+    $media->save();
+    $this->assertEndpoint($media, 'mid', 'media', $roles);
 
     // 3. GET, POST, PATCH request to taxonomy_term Resource type.
     $vocab = Vocabulary::load('tags');
     $term = $this->createTerm($vocab);
-    $this->assertEndpoint($term, 'tid', 'taxonomy_term');
+    $this->assertEndpoint($term, 'tid', 'taxonomy_term', $roles);
   }
 
   /**
    * Asserts the GET, POST and PATCH method for the provided resources.
    *
-   * @param mixed $resource
+   * @param \Drupal\Core\Entity\EntityInterface $resource
    *   An object containing all the information about the resource.
    * @param string $id
    *   Name of the ID field of the resource.
    * @param string $entity_type_id
    *   Id of the entity_type to which resource belongs to.
+   * @param string[]|null $roles
+   *   The user role(s) to test with, or NULL to test as an anonymous user. If
+   *   this is an empty array, the test will run as an authenticated user with
+   *   no additional roles.
    */
-  protected function assertEndpoint($resource, string $id, string $entity_type_id) : void {
+  protected function assertEndpoint(EntityInterface $resource, string $id, string $entity_type_id, ?array $roles) : void {
 
     $account = $this->drupalCreateUser();
-    $account->addRole('administrator');
+    $account->addRole($roles);
     $account->save();
 
     // Assert GET, POST, PATCH request for the given Resource type.
-    $resource_url = Url::fromUri('base:/jsonapi/' . $entity_type_id . '/' . $resource->bundle());
+    $resource_url = Url::fromUri("base:/jsonapi/{$entity_type_id}/{$resource->bundle()}");
     // Assert that GET request is allowed.
     $response = $this->request('GET', $resource_url, []);
     $this->assertEquals(200, $response->getStatusCode());
@@ -100,6 +124,14 @@ class JsonApiTest extends BrowserTestBase {
     foreach ($created_response['data'] as $item) {
       $this->assertFalse(array_key_exists('drupal_internal__' . $id, $item['attributes']));
     }
+
+    // Assert that GET request is allowed for individual resources.
+    $individual_resource_url = Url::fromUri("base:/jsonapi/{$entity_type_id}/{$resource->bundle()}/{$resource->uuid()}");
+    $response = $this->request('GET', $individual_resource_url, []);
+    $this->assertEquals(200, $response->getStatusCode());
+    $created_response = Json::decode($response->getBody()->__toString());
+    // Assert that nid field is disabled.
+    $this->assertFalse(array_key_exists('drupal_internal__' . $id, $created_response['data']['attributes']));
 
     $body = [
       'data' => [
@@ -223,6 +255,26 @@ class JsonApiTest extends BrowserTestBase {
       'headers' => ['Content-Type' => 'application/vnd.api+json'],
     ]);
     $this->assertEquals(404, $response->getStatusCode());
+  }
+
+  /**
+   * Data provider for ::testNonRestrictedEndpoints().
+   *
+   * @return array[]
+   *   Sets of arguments to pass to the test method.
+   */
+  public function providerNonRestrictedEndpoints() {
+    return [
+      'anonymous user' => [
+        NULL,
+      ],
+      'authenticated user' => [
+        [],
+      ],
+      'administrator' => [
+        ['administrator'],
+      ],
+    ];
   }
 
 }
