3361ca379c5b6118249dced9a65c26699ddc5d8c
bhadradisha <67892280+bhadradisha@users.noreply.github.com>
2020-09-04 04:14:10 +0530

    ACMS-297: Update date in horizontal content template and add test coverage for recent articles and past/upcoming events blocks (#248)

---
+++ modules/acquia_cms_event/tests/src/ExistingSite/PastEventsBlockTest.php
@@ -0,0 +1,96 @@
+<?php
+
+namespace Drupal\Tests\acquia_cms_event\ExistingSite;
+
+use Behat\Mink\Element\ElementInterface;
+use Drupal\Tests\acquia_cms_common\Traits\AssertLinksTrait;
+use Drupal\Tests\block\Traits\BlockCreationTrait;
+use weitzman\DrupalTestTraits\ExistingSiteBase;
+
+/**
+ * Tests the past events block on page.
+ *
+ * @group acquia_cms
+ * @group acquia_cms_event
+ */
+class PastEventsBlockTest extends ExistingSiteBase {
+
+  use AssertLinksTrait;
+  use BlockCreationTrait;
+
+  /**
+   * {@inheritdoc}
+   */
+  protected function setUp() {
+    parent::setUp();
+    $block = $this->placeBlock('views_block:event_cards-past_events_block', [
+      'region' => 'content',
+      'id' => 'past_events_block',
+    ]);
+    $this->markEntityForCleanup($block);
+
+    $this->createNode([
+      'type' => 'event',
+      'title' => 'Event Example 1',
+      'field_event_start' => '2020-06-03T22:00:00',
+      'field_event_end' => '2020-06-09T12:00:00',
+      'moderation_state' => 'published',
+    ]);
+    $this->createNode([
+      'type' => 'event',
+      'title' => 'Event Example 2',
+      'field_event_start' => '2020-07-13T22:00:00',
+      'field_event_end' => '2020-07-16T12:00:00',
+      'moderation_state' => 'published',
+    ]);
+    $this->createNode([
+      'type' => 'event',
+      'title' => 'Event Example 3',
+      'field_event_start' => '2020-07-03T22:00:00',
+      'field_event_end' => '2020-07-03T12:00:00',
+      'moderation_state' => 'published',
+    ]);
+    $this->createNode([
+      'type' => 'event',
+      'title' => 'Event Example 4',
+      'field_event_start' => '2020-08-03T22:00:00',
+      'field_event_end' => '2020-08-03T12:00:00',
+      'moderation_state' => 'draft',
+    ]);
+  }
+
+  /**
+   * Tests the past event block.
+   */
+  public function testPastEventsBlock() {
+    $this->drupalGet('');
+    $this->assertSession()->pageTextContains('Past Events');
+    $this->assertLinksExistInOrder();
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  protected function getLinks(): array {
+    $links = $this->getSession()
+      ->getPage()
+      ->findAll('css', '#block-past-events-block .view-event-cards .coh-container .coh-heading');
+
+    $map = function (ElementInterface $link) {
+      return $link->getText();
+    };
+    return array_map($map, $links);
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  protected function getExpectedLinks() : array {
+    return [
+      'Event Example 2',
+      'Event Example 3',
+      'Event Example 1',
+    ];
+  }
+
+}
