d533f181cae27d32d642b003c356c76edff3ef35
Chandan Singh <63041696+chandan-singh7929@users.noreply.github.com>
2020-09-03 17:06:44 +0530

    ACMS-303: Add padding to text component (#250)

---
+++ tests/src/ExistingSiteJavascript/TextComponentTest.php
@@ -0,0 +1,46 @@
+<?php
+
+namespace Drupal\Tests\acquia_cms\ExistingSiteJavascript;
+
+/**
+ * Tests the Text component.
+ *
+ * @group acquia_cms
+ */
+class TextComponentTest extends CohesionTestBase {
+
+  /**
+   * Test that the Text component can be added to a layout canvas.
+   */
+  public function testComponentInstalled() {
+    $account = $this->createUser();
+    $account->addRole('administrator');
+    $account->save();
+    $this->drupalLogin($account);
+
+    $this->drupalGet('/node/add/page');
+
+    // Add the component to the layout canvas.
+    $canvas = $this->waitForElementVisible('css', '.coh-layout-canvas');
+    $this->addComponent($canvas, 'Text');
+  }
+
+  /**
+   * Tests that component can be edited by a specific user role.
+   *
+   * @param string $role
+   *   The ID of the user role to test with.
+   *
+   * @dataProvider providerEditAccess
+   */
+  public function testEditAccess(string $role) {
+    $account = $this->createUser();
+    $account->addRole($role);
+    $account->save();
+    $this->drupalLogin($account);
+
+    $this->drupalGet('/admin/cohesion/components/components');
+    $this->editComponentDefinition('General components', 'Text');
+  }
+
+}
