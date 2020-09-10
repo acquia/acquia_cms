f60305ac83040b10723621a876633c3ef89ccee1
Michael Sherron <msherron@gmail.com>
2020-09-10 11:19:12 -0700

    ACMS-80: Clearing cache first in site-install script to avoid memcache collision.

--- hooks/common/post-code-deploy/reinstall.sh
+++ hooks/common/post-code-deploy/reinstall.sh
@@ -11,5 +11,6 @@ site="$1"
 target_env="$2"
 
 # Fresh install of Acquia CMS.
+/usr/local/bin/drush9 @$site.$target_env cr
 /usr/local/bin/drush9 @$site.$target_env site-install acquia_cms --account-pass=admin --yes
 /usr/local/bin/drush9 @$site.$target_env pm-enable acquia_cms_example --yes
