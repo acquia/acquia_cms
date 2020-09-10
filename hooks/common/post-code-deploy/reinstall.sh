65519ebea0902e19c87b64ea326ae5eff95b7c59
Michael Sherron <msherron@gmail.com>
2020-09-10 11:27:30 -0700

    ACMS-80: Forgot post-code-deploy hook.

--- hooks/common/post-code-deploy/reinstall.sh
+++ hooks/common/post-code-deploy/reinstall.sh
@@ -10,7 +10,8 @@ drush --version
 site="$1"
 target_env="$2"
 
-# Fresh install of Acquia CMS.
+# Fresh install of Acquia CMS. We need to clear cache first in case memcache is
+# enabled, else there will be a collision on site install.
 /usr/local/bin/drush9 @$site.$target_env cr
 /usr/local/bin/drush9 @$site.$target_env site-install acquia_cms --account-pass=admin --yes
 /usr/local/bin/drush9 @$site.$target_env pm-enable acquia_cms_example --yes
