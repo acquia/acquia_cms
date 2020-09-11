44eadf668c0f3cd1ec480e37a586e3966db3bfba
Mike Madison <mike.madison@acquia.com>
2020-09-11 15:28:34 -0700

    ACMS-361: relocating pa11y to test script.

--- tests/travis/script.sh
+++ tests/travis/script.sh
@@ -1,21 +1,13 @@
 #!/usr/bin/env bash
 
-# NAME
-#     after_success.sh - Perform post-success tasks.
-#
-# SYNOPSIS
-#     after_success.sh
-#
-# DESCRIPTION
-#     Conditionally sends code coverage data to Coveralls.
-
 cd "$(dirname "$0")"
 
 # Reuse ORCA's own includes.
 source ../../../orca/bin/travis/_includes.sh
 
 # Run ORCA's standard post-success script.
-../../../orca/bin/travis/after_success.sh
+../../../orca/bin/travis/script.sh
+
 
 cd $ORCA_FIXTURE_DIR
 drush pm-enable --yes acquia_cms_demo_pubsec
