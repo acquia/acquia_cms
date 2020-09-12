979e3381c4a7e136274655eb9d61bc109453bb78
Phéna Proxima <adam@phenaproxima.net>
2020-09-11 20:08:30 -0400

    ACMS-361: Clean up .gitignore and restore comments in script.sh

--- tests/travis/script.sh
+++ tests/travis/script.sh
@@ -1,13 +1,21 @@
 #!/usr/bin/env bash
 
+# NAME
+#     script.sh - Run ORCA tests.
+#
+# SYNOPSIS
+#     script.sh
+#
+# DESCRIPTION
+#     Runs static code analysis and automated tests.
+
 cd "$(dirname "$0")"
 
 # Reuse ORCA's own includes.
 source ../../../orca/bin/travis/_includes.sh
 
-# Run ORCA's standard post-success script.
+# Run ORCA's standard script.
 ../../../orca/bin/travis/script.sh
-
 
 cd $ORCA_FIXTURE_DIR
 drush pm-enable --yes acquia_cms_demo_pubsec
