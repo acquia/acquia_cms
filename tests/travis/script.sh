e658e5ccc92beaccaaf81236efaeb03a36428f13
Mike Madison <22753451+mikemadison13@users.noreply.github.com>
2020-09-11 21:20:49 -0700

    ACMS-361: Run pa11y in CI (#282)

---
+++ tests/travis/script.sh
@@ -0,0 +1,35 @@
+#!/usr/bin/env bash
+
+# NAME
+#     script.sh - Run ORCA tests.
+#
+# SYNOPSIS
+#     script.sh
+#
+# DESCRIPTION
+#     Runs static code analysis and automated tests.
+
+cd "$(dirname "$0")"
+
+# Reuse ORCA's own includes.
+source ../../../orca/bin/travis/_includes.sh
+
+# Run ORCA's standard script.
+../../../orca/bin/travis/script.sh
+
+# If there is no fixture, there's nothing else for us to do.
+[[ -d "$ORCA_FIXTURE_DIR" ]] || exit 0
+
+# TODO: Delete this line when we require pa11y to pass.
+exit 0
+
+cd $ORCA_FIXTURE_DIR
+# Install the demo site so we have a lot of material to run through
+# pa11y.
+drush pm-enable --yes acquia_cms_demo_pubsec
+
+# Install npm dependencies and run pa11y.
+cd $TRAVIS_BUILD_DIR
+npm install
+orca fixture:run-server &
+npm run tests
