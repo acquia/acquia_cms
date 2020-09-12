fc031369db1f7592d9a05d67f1486774317db5f8
Phéna Proxima <adam@phenaproxima.net>
2020-09-11 20:17:23 -0400

    ACMS-361: Fix wrong conditional

--- tests/travis/script.sh
+++ tests/travis/script.sh
@@ -17,12 +17,16 @@ source ../../../orca/bin/travis/_includes.sh
 # Run ORCA's standard script.
 ../../../orca/bin/travis/script.sh
 
-case "$ORCA_JOB" in
-  cd $ORCA_FIXTURE_DIR
-  drush pm-enable --yes acquia_cms_demo_pubsec
+# If there is no fixture, there's nothing else for us to do.
+[[ -d "$ORCA_FIXTURE_DIR" ]] || exit 0
 
-  cd $TRAVIS_BUILD_DIR
-  npm install
-  orca fixture:run-server &
-  npm run tests
-esac
+cd $ORCA_FIXTURE_DIR
+# Install the demo site so we have a lot of material to run through
+# pa11y.
+drush pm-enable --yes acquia_cms_demo_pubsec
+
+# Install npm dependencies and run pa11y.
+cd $TRAVIS_BUILD_DIR
+npm install
+orca fixture:run-server &
+npm run tests
