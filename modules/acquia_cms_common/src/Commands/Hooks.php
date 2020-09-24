7cacb405e2a42dd16ed9b5a9aea8223643d8248f
dishabhadra <disha.bhadra@acquia.com>
2020-09-24 04:41:59 -0400

    ACMS-336: removing drush patch and adding validate hook.

--- modules/acquia_cms_common/src/Commands/Hooks.php
+++ modules/acquia_cms_common/src/Commands/Hooks.php
@@ -3,12 +3,30 @@
 namespace Drupal\acquia_cms_common\Commands;
 
 use Consolidation\AnnotatedCommand\CommandData;
+use Drupal\Core\Extension\ModuleHandlerInterface;
 use Drush\Commands\DrushCommands;
 
 /**
  * Implements Drush command hooks.
  */
 final class Hooks extends DrushCommands {
+
+  /**
+   * The module handler.
+   *
+   * @var \Drupal\Core\Extension\ModuleHandlerInterface
+   */
+  protected $moduleHandler;
+
+  /**
+   * Constructs a WebformSubmissionLogRouteSubscriber object.
+   *
+   * @param \Drupal\Core\Extension\ModuleHandlerInterface $module_handler
+   *   The module handler.
+   */
+  public function __construct(ModuleHandlerInterface $module_handler) {
+    $this->moduleHandler = $module_handler;
+  }
 
   /**
    * Alters the result of the config:get command.
@@ -22,6 +40,41 @@ final class Hooks extends DrushCommands {
       unset($result['uuid'], $result['_core']);
     }
     return $result;
+  }
+
+  /**
+   * Validate the result of the pm:enable command.
+   *
+   * @hook validate pm:enable
+   */
+  public function validateRequirement(CommandData $commandData) {
+    $modules = $commandData->input()->getArgument('modules');
+    // Run requirements checks on each module.
+    // @see \drupal_check_module()
+    require_once DRUSH_DRUPAL_CORE . '/includes/install.inc';
+    foreach ($modules as $module) {
+      module_load_install($module);
+      $requirements = $this->moduleHandler->invoke($module, 'requirements', ['install']);
+      if (is_array($requirements) && drupal_requirements_severity($requirements) == REQUIREMENT_ERROR) {
+        $reasons = [];
+        // Print any error messages.
+        foreach ($requirements as $id => $requirement) {
+          if (isset($requirement['severity']) && $requirement['severity'] == REQUIREMENT_ERROR) {
+            $message = $requirement['description'];
+            if (isset($requirement['value']) && $requirement['value']) {
+              $message = dt('@requirements_message (Currently using @item version @version)',
+              [
+                '@requirements_message' => $requirement['description'],
+                '@item' => $requirement['title'],
+                '@version' => $requirement['value'],
+              ]);
+            }
+            $reasons[$id] = "$module: " . (string) $message;
+          }
+        }
+        throw new \Exception(implode("\n", $reasons));
+      }
+    }
   }
 
 }
