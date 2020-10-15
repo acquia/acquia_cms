2a0c18ac2d029a4dd1c20fab37edc28b865b5024
Kunal Singh <kunal.singh@acquia.com>
2020-10-15 13:37:58 -0400

    ACMS-319: Created the clear all facets block

---
+++ modules/acquia_cms_search/src/Plugin/Block/ClearAllFacet.php
@@ -0,0 +1,85 @@
+<?php
+
+namespace Drupal\acquia_cms_search\Plugin\Block;
+
+use Drupal\Core\Block\BlockBase;
+use Drupal\Core\Block\BlockPluginInterface;
+use Drupal\Core\Cache\Cache;
+use Drupal\Core\Link;
+use Drupal\Core\Plugin\ContainerFactoryPluginInterface;
+use Drupal\Core\Routing\CurrentRouteMatch;
+use Symfony\Component\DependencyInjection\ContainerInterface;
+
+/**
+ * Provides a 'ClearAllFacet' Block.
+ *
+ * @Block(
+ *   id = "clearall_facet",
+ *   admin_label = @Translation("Clear All Facets"),
+ * )
+ */
+class ClearAllFacet extends BlockBase implements BlockPluginInterface, ContainerFactoryPluginInterface {
+
+  /**
+   * The config factory object.
+   *
+   * @var \Drupal\Core\Routing\CurrentRouteMatch
+   */
+  protected $routematch;
+
+  /**
+   * Constructs the ClearAllFacet Block.
+   *
+   * @param array $configuration
+   *   The plugin configuration, i.e. an array with configuration values keyed
+   *   by configuration option name. The special key 'context' may be used to
+   *   initialize the defined contexts by setting it to an array of context
+   *   values keyed by context names.
+   * @param string $plugin_id
+   *   The plugin_id for the plugin instance.
+   * @param mixed $plugin_definition
+   *   The plugin implementation definition.
+   * @param \Drupal\Core\Routing\CurrentRouteMatch $route_match
+   *   The config factory.
+   */
+  public function __construct(array $configuration, $plugin_id, $plugin_definition, CurrentRouteMatch $route_match) {
+    parent::__construct($configuration, $plugin_id, $plugin_definition);
+
+    $this->routematch = $route_match;
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public static function create(ContainerInterface $container, array $configuration, $plugin_id, $plugin_definition) {
+    return new static(
+      $configuration,
+      $plugin_id,
+      $plugin_definition,
+      $container->get('current_route_match')
+    );
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function build() {
+    // Check if any facets_query is present in the url. If yes then display the
+    // reset link.
+    if ($this->routematch->getParameter('facets_query')) {
+      $url = $this->routematch->getRouteName();
+      $link = Link::createFromRoute($this->t('Clear filter(s)'), $url);
+
+      return $link->toRenderable();
+    }
+    return [];
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function getCacheContexts() {
+    return Cache::mergeContexts(parent::getCacheContexts(), ['url']);
+  }
+
+}
