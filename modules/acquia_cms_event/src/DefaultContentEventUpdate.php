35df283d00c0906539ae7f304700478076b9e009
kunal-singh13 <67369064+kunal-singh13@users.noreply.github.com>
2020-11-06 19:54:15 +0530

    ACMS-546: Added the logic to update duration field in event (#509)
    * ACMS-546: Added the logic to update duration field in event
    
    * ACMS-546: Updated the default content node for better understanding of duration
    
    * ACMS-546: Updated the door time logic for better understanding
    
    * ACMS-546: Added logic for duration text[skip ci]

--- modules/acquia_cms_event/src/DefaultContentEventUpdate.php
+++ modules/acquia_cms_event/src/DefaultContentEventUpdate.php
@@ -17,7 +17,7 @@ class DefaultContentEventUpdate {
    */
   public function getUpdatedDates(array $date_time) : array {
     if (strtotime($date_time['start_date']) < strtotime('+30 days') ||
-      strtotime($date_time['start_date']) > strtotime($date_time['end_date'])) {
+      (!empty($date_time['end_date']) && strtotime($date_time['start_date']) > strtotime($date_time['end_date']))) {
       $date_time['start_date'] = date('Y-m-d', strtotime('+30 days'));
     }
 
@@ -27,11 +27,9 @@ class DefaultContentEventUpdate {
         $date_time['end_date'] = date('Y-m-d', strtotime('+31 days'));
       }
     }
+    // Door time will always be same as start_date.
+    $date_time['door_time'] = $date_time['start_date'];
 
-    if (strtotime($date_time['door_time']) > strtotime($date_time['end_date']) ||
-      strtotime($date_time['door_time']) < strtotime('+30 days')) {
-      $date_time['door_time'] = date('Y-m-d', strtotime('+30 days'));
-    }
     return $date_time;
 
   }
@@ -48,6 +46,17 @@ class DefaultContentEventUpdate {
     $entity->set('field_event_start', date('Y-m-d\T' . $entity->get('field_event_start')->date->format('H:i:s'), strtotime($updated_data['start_date'])));
     if (!empty($updated_data['end_date'])) {
       $entity->set('field_event_end', date('Y-m-d\T' . $entity->get('field_event_end')->date->format('H:i:s'), strtotime($updated_data['end_date'])));
+      // Updating the duration field based on start and end date of event.
+      $time_diff = date_diff(
+        new \DateTime($entity->get('field_event_end')->value),
+        new \DateTime($entity->get('field_event_start')->value));
+
+      $day = $time_diff->d > 1 ? 'days' : 'day';
+      $hour = $time_diff->h > 1 ? 'hours' : 'hour';
+      $minute = $time_diff->i > 1 ? 'minutes' : 'minute';
+      $entity->set(
+        'field_event_duration',
+        $time_diff->format("%a " . $day . ", %h " . $hour . ", %i " . $minute));
     }
     $entity->set('field_door_time', date('Y-m-d\T' . $entity->get('field_door_time')->date->format('H:i:s'), strtotime($updated_data['door_time'])));
 
