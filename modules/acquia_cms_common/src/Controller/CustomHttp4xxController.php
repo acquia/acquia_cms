705c8f9effbfd9a04e9f7e1cc7100820c6e6bcfa
Ganesh  Devkate <ganeshdevkate@gmail.com>
2020-11-24 10:16:35 +0530

    ACMS-518: Adjust button arrows for 404 PubSec design.

--- modules/acquia_cms_common/src/Controller/CustomHttp4xxController.php
+++ modules/acquia_cms_common/src/Controller/CustomHttp4xxController.php
@@ -5,33 +5,9 @@ namespace Drupal\acquia_cms_common\Controller;
 use Drupal\Core\Controller\ControllerBase;
 
 /**
- * Controller for default HTTP 4xx responses.
+ * Controller for default HTTP 403 and 404 responses.
  */
 class CustomHttp4xxController extends ControllerBase {
-
-  /**
-   * The default 4xx error content.
-   *
-   * @return array
-   *   A render array containing the message to display for 4xx errors.
-   */
-  public function on4xx() {
-    return [
-      '#markup' => $this->t('A client error happened'),
-    ];
-  }
-
-  /**
-   * The default 401 content.
-   *
-   * @return array
-   *   A render array containing the message to display for 401 pages.
-   */
-  public function on401() {
-    return [
-      '#markup' => $this->t('Please log in to access this page.'),
-    ];
-  }
 
   /**
    * The default 403 content.
