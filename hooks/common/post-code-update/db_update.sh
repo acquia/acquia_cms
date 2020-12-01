465dad9ac9796973ec4811e3b638070cbee8baec
saurabh tripathi <saurabh.tripathi@acquia.com>
2020-12-01 13:13:10 +0530

    ACMS-567: Automatic Database Updates in ACE with Cloud Hooks

--- hooks/common/post-code-update/db_update.sh
+++ hooks/common/post-code-update/db_update.sh
@@ -2,7 +2,7 @@
 #
 # Cloud Hook: Reinstall Acquia CMS
 #
-# Run `drush site-install acquia_cms` in the target environment.
+# Run `drush updatedb acquia_cms` in the target environment.
 
 which drush
 drush --version
@@ -13,5 +13,5 @@ target_env="$2"
 # Fresh install of Acquia CMS. We need to clear cache first in case memcache is
 # enabled, else there will be a collision on site install.
 /usr/local/bin/drush9 @$site.$target_env cr
-/usr/local/bin/drush9 @$site.$target_env site-install acquia_cms --account-pass=admin --yes
+/usr/local/bin/drush9 @$site.$target_env updatedb --no-interaction
 /usr/local/bin/drush9 @$site.$target_env pm-enable acquia_cms_development --yes
