4db807d72ece2bed59e73b54bdcccbe2dcfd7ecb
kdruckman <kdruckman@gmail.com>
2020-12-08 16:53:47 -0600

    replace fixture.

--- tests/travis/install.sh
+++ tests/travis/install.sh
@@ -39,6 +39,7 @@ if [[ "$ACMS_JOB" == "base" ]] && [[ -n "$ACMS_DB_ARTIFACT" ]] && [[ -n "$ACMS_F
     tar -xzvf $ACMS_FILES_ARTIFACT
     gunzip $ACMS_DB_ARTIFACT
     drush sql:cli < $TRAVIS_BUILD_DIR/tests/acms.sql
+    mkdir -p config/default
     drush updatedb --yes -vvv
 fi
 
