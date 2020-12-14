66348e51e48fd56040e3ab1b8105efcac04ec2b9
saurabh tripathi <saurabh.tripathi@acquia.com>
2020-12-14 11:39:59 +0530

    ACMS-445: Update demo page content for acms starter and pubsec demo

--- modules/acquia_cms_event/tests/src/Unit/DefaultContentUpdateEventTest.php
+++ modules/acquia_cms_event/tests/src/Unit/DefaultContentUpdateEventTest.php
@@ -40,9 +40,9 @@ class DefaultContentUpdateEventTest extends UnitTestCase {
       'door_time' => date('Y-m-d', strtotime('-15 days')),
     ];
     $updated_event = $this->updateEvent->getUpdatedDates($pastEvent);
-    $this->assertEquals(date('Y-m-d', strtotime("30 days")), $updated_event['start_date']);
-    $this->assertEquals(date('Y-m-d', strtotime("31 days")), $updated_event['end_date']);
-    $this->assertEquals(date('Y-m-d', strtotime("30 days")), $updated_event['door_time']);
+    $this->assertEquals(date('Y-m-d', strtotime($pastEvent['start_date'] . '+30 days')), $updated_event['start_date']);
+    $this->assertEquals(date('Y-m-d', strtotime($pastEvent['start_date'] . '+31 days')), $updated_event['end_date']);
+    $this->assertEquals(date('Y-m-d', strtotime($pastEvent['start_date'] . '+30 days')), $updated_event['door_time']);
     // Asserting when event in past and door time greater than event end time.
     $pastEvent = [
       'start_date' => date('Y-m-d', strtotime('-15 days')),
@@ -50,19 +50,10 @@ class DefaultContentUpdateEventTest extends UnitTestCase {
       'door_time' => date('Y-m-d', strtotime('+15 days')),
     ];
     $updated_event = $this->updateEvent->getUpdatedDates($pastEvent);
-    $this->assertEquals(date('Y-m-d', strtotime("30 days")), $updated_event['start_date']);
-    $this->assertEquals(date('Y-m-d', strtotime("31 days")), $updated_event['end_date']);
-    $this->assertEquals(date('Y-m-d', strtotime("30 days")), $updated_event['door_time']);
-    // Asserting when start date is greater than end date.
-    $pastEvent = [
-      'start_date' => date('Y-m-d', strtotime('+36 days')),
-      'end_date' => date('Y-m-d', strtotime('-18 days')),
-      'door_time' => date('Y-m-d', strtotime('+15 days')),
-    ];
-    $updated_event = $this->updateEvent->getUpdatedDates($pastEvent);
-    $this->assertEquals(date('Y-m-d', strtotime("30 days")), $updated_event['start_date']);
-    $this->assertEquals(date('Y-m-d', strtotime("31 days")), $updated_event['end_date']);
-    $this->assertEquals(date('Y-m-d', strtotime("30 days")), $updated_event['door_time']);
+    $this->assertEquals(date('Y-m-d', strtotime($pastEvent['start_date'] . '+30 days')), $updated_event['start_date']);
+    $this->assertEquals(date('Y-m-d', strtotime($pastEvent['start_date'] . '+31 days')), $updated_event['end_date']);
+    $this->assertEquals(date('Y-m-d', strtotime($pastEvent['start_date'] . '+30 days')), $updated_event['door_time']);
+
     // Asserting when event is in future.
     $pastEvent = [
       'start_date' => date('Y-m-d', strtotime('+36 days')),
