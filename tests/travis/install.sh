4e3c6afdf7e6fb5cbf5d55a4fbb2d77ac4f1a3be
kdruckman <kdruckman@gmail.com>
2020-12-16 08:50:00 -0600

    ACMS-568: Try symlinking profile

--- tests/travis/install.sh
+++ tests/travis/install.sh
@@ -34,6 +34,11 @@ if [ ! -z $COHESION_ARTIFACT ] && [ -f $COHESION_ARTIFACT ]; then
   drush config:import --yes --partial --source sites/default/files/cohesion/config
 fi
 
+cd docroot/profiles
+ln -s contrib/acquia_cms
+# Ensure the symlinks are included in the ORCA fixture snapshot.
+git add .
+
 if [[ "$ACMS_JOB" == "base" ]] && [[ -n "$ACMS_DB_ARTIFACT" ]] && [[ -n "$ACMS_FILES_ARTIFACT" ]] && [[ -f "$ACMS_DB_ARTIFACT" ]] && [[ -f "$ACMS_FILES_ARTIFACT" ]]; then
     echo "Installing From Artifacts"
     tar -xzf $ACMS_FILES_ARTIFACT
