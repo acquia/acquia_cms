44aa9204e7f58edc838033b2a6ab68fd9d8db339
kdruckman <kdruckman@gmail.com>
2021-01-29 17:38:52 -0600

    ACMS-572: Update script.

--- tests/travis/script.sh
+++ tests/travis/script.sh
@@ -20,7 +20,8 @@ if [ "$ACMS_JOB" == "base_full" ]; then
 fi
 
 if [ "$ACMS_JOB" == "base" ]; then
-eval './vendor/bin/phpunit --verbose --colors=always --debug --exclude-group=orca_ignore,low_risk --testsuite=orca'
+/home/travis/build/acquia/orca/bin/orca fixture:status
+/home/travis/build/acquia/orca-build/vendor/bin/phpunit --verbose --colors=always --debug --exclude-group=orca_ignore,low_risk --configuration=/home/travis/build/acquia/orca-build/docroot/core/phpunit.xml --testsuite=orca
 fi
 
 # If there is no fixture, there's nothing else for us to do.
