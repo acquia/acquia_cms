060a3cc97452ada806c16765c80e47dd033f0455
Gaurav Goyal <gaurav.goyal@acquia.com>
2021-02-25 16:49:55 +0530

    ACMS-651: Acquia Google Maps APIrefactored.

--- modules/acquia_cms_tour/src/Form/GoogleAnalyticsForm.php
+++ modules/acquia_cms_tour/src/Form/GoogleAnalyticsForm.php
@@ -2,26 +2,13 @@
 
 namespace Drupal\acquia_cms_tour\Form;
 
-use Drupal\Core\Extension\InfoParserInterface;
-use Drupal\Core\Extension\ModuleHandlerInterface;
-use Drupal\Core\Form\ConfigFormBase;
 use Drupal\Core\Form\FormStateInterface;
-use Drupal\Core\State\StateInterface;
 use Drupal\Core\Url;
-use Drupal\Core\Utility\LinkGeneratorInterface;
-use Symfony\Component\DependencyInjection\ContainerInterface;
 
 /**
  * Provides a form to configure the Google Analytics module.
  */
-final class GoogleAnalyticsForm extends ConfigFormBase implements AcquiaDashboardInterface {
-
-  /**
-   * The state service.
-   *
-   * @var \Drupal\Core\State\StateInterface
-   */
-  protected $state;
+final class GoogleAnalyticsForm extends AcquiaCMSDashboardBase {
 
   /**
    * Provides module name.
@@ -29,58 +16,6 @@ final class GoogleAnalyticsForm extends ConfigFormBase implements AcquiaDashboar
    * @var string
    */
   protected $module = 'google_analytics';
-
-  /**
-   * The module handler.
-   *
-   * @var \Drupal\Core\Extension\ModuleHandlerInterface
-   */
-  protected $moduleHandler;
-
-  /**
-   * The link generator.
-   *
-   * @var \Drupal\Core\Utility\LinkGeneratorInterface
-   */
-  protected $linkGenerator;
-
-  /**
-   * The info file parser.
-   *
-   * @var \Drupal\Core\Extension\InfoParserInterface
-   */
-  protected $infoParser;
-
-  /**
-   * Constructs a new GoogleAnalyticsForm.
-   *
-   * @param \Drupal\Core\Extension\ModuleHandlerInterface $module_handler
-   *   The module handler service.
-   * @param \Drupal\Core\Utility\LinkGeneratorInterface $link_generator
-   *   The link generator.
-   * @param \Drupal\Core\Extension\InfoParserInterface $info_parser
-   *   The info file parser.
-   * @param \Drupal\Core\State\StateInterface $state
-   *   The state service.
-   */
-  public function __construct(ModuleHandlerInterface $module_handler, LinkGeneratorInterface $link_generator, InfoParserInterface $info_parser, StateInterface $state) {
-    $this->module_handler = $module_handler;
-    $this->linkGenerator = $link_generator;
-    $this->infoParser = $info_parser;
-    $this->state = $state;
-  }
-
-  /**
-   * {@inheritdoc}
-   */
-  public static function create(ContainerInterface $container) {
-    return new static(
-      $container->get('module_handler'),
-      $container->get('link_generator'),
-      $container->get('info_parser'),
-      $container->get('state')
-    );
-  }
 
   /**
    * {@inheritdoc}
@@ -104,19 +39,21 @@ final class GoogleAnalyticsForm extends ConfigFormBase implements AcquiaDashboar
   public function buildForm(array $form, FormStateInterface $form_state) {
     $form['#tree'] = FALSE;
     $module = $this->module;
-    if ($this->module_handler->moduleExists('google_analytics')) {
+    if ($this->isModuleEnabled()) {
       $account = $this->config('google_analytics.settings')->get('account');
+
+      $configured = $this->getProgressState();
       if (!empty($account)) {
-        $this->state->set('google_analytics_progress', TRUE);
+        $configured = TRUE;
+        $this->setState();
       }
-      if ($this->state->get('google_analytics_progress')) {
+      if ($configured) {
         $form['check_icon'] = [
           '#prefix' => '<span class= "dashboard-check-icon">',
           '#suffix' => "</span>",
         ];
       }
-    }
-    if ($this->module_handler->moduleExists($module)) {
+
       $module_path = $this->module_handler->getModule($module)->getPathname();
       $module_info = $this->infoParser->parse($module_path);
       $form[$module] = [
@@ -179,31 +116,16 @@ final class GoogleAnalyticsForm extends ConfigFormBase implements AcquiaDashboar
     $this->config('google_analytics.settings')->set('account', $property_id)->save();
     $this->state->set('google_analytics_progress', TRUE);
     $this->messenger()->addStatus('The configuration options have been saved.');
+
+    // Update state.
+    $this->setState();
   }
 
   /**
    * {@inheritdoc}
    */
   public function ignoreConfig(array &$form, FormStateInterface $form_state) {
-    $this->state->set('google_analytics_progress', TRUE);
-  }
-
-  /**
-   * {@inheritdoc}
-   */
-  public function getModuleStatus() {
-    if ($this->module_handler->moduleExists($this->module)) {
-      return TRUE;
-    }
-  }
-
-  /**
-   * {@inheritdoc}
-   */
-  public function getProgressState() {
-    if ($this->module_handler->moduleExists($this->module)) {
-      return $this->state->get('google_analytics_progress');
-    }
+    $this->setState();
   }
 
 }
