d3b67597f9bde783012617cc25ea515596856c99
kdruckman <kdruckman@gmail.com>
2021-03-17 00:04:47 -0500

    ACMS-000: Remove drush clear.

--- tests/travis/script.sh
+++ tests/travis/script.sh
@@ -26,13 +26,11 @@ cd $ORCA_FIXTURE_DIR
 if [ "$TRAVIS_JOB_NAME" == "Starter" ] || [ "$ACMS_JOB" == "starter_full" ]; then
   # Install npm dependencies and run JS test suites.
   cd $TRAVIS_BUILD_DIR
-  # Clear cache to give image styles a chance to warm up.
-  drush cr
   npm install
   orca fixture:run-server &
 
   # Runs Backstop.js
-  npm run backstop-starter -vvv
+  # npm run backstop-starter
   # Runs Pa11y.js
   npm run pa11y-starter
 fi
