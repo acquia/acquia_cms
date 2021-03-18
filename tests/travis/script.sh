bb642e5b593bcede19ed9e995a9f682b7b8053e3
kdruckman <kdruckman@gmail.com>
2021-03-18 14:10:59 -0500

    ACMS-698: Update backstop references.

--- tests/travis/script.sh
+++ tests/travis/script.sh
@@ -30,7 +30,7 @@ if [ "$TRAVIS_JOB_NAME" == "Starter" ] || [ "$ACMS_JOB" == "starter_full" ]; the
   orca fixture:run-server &
 
   # Runs Backstop.js
-  # npm run backstop-starter
+  npm run backstop-starter
   # Runs Pa11y.js
   # npm run pa11y-starter
 fi
