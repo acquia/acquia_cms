e8a0beda723dc2be2a4d31485e4df89750c38200
Chandan Singh <chandan.singh@acquia.com>
2021-05-12 20:07:16 +0530

    ACMS-766: Update acquia_cms_starter module to support atomic nature

--- modules/acquia_cms_common/src/AcmsModulesUninstallValidator.php
+++ modules/acquia_cms_common/src/AcmsModulesUninstallValidator.php
@@ -53,16 +53,47 @@ class AcmsModulesUninstallValidator implements ModuleUninstallValidatorInterface
       'acquia_cms_video',
     ];
     if (in_array($module, $allowed_modules) && $this->hasContent($type)) {
-      $reasons[] = $this->t('There is content available for node type [@type], please manually delete content before uninstallation.', [
+      $reasons[] = $this->t('There are content available for node type [@type], please manually delete content before uninstallation.', [
         '@type' => $type,
       ]);
     }
     elseif (in_array($module, $allowed_media_modules)  && $this->hasMedia($type)) {
-      $reasons[] = $this->t('There is media available for type [@type], please manually delete content before uninstallation.', [
+      $reasons[] = $this->t('There are media available for type [@type], please manually delete content before uninstallation.', [
+        '@type' => $type,
+      ]);
+    }
+    elseif ($module == 'acquia_cms_starter'  && $type = $this->hasMediaAndContent()) {
+      $reasons[] = $this->t('There are content/media available for type [@type], please manually delete content before uninstallation.', [
         '@type' => $type,
       ]);
     }
     return $reasons;
+  }
+
+  /**
+   * Get entity type id if content/media available.
+   */
+  private function hasMediaAndContent() {
+    $node_types = ['article', 'page', 'event', 'person', 'place'];
+    $media_types = ['document', 'image', 'vide'];
+    foreach ($node_types as $type) {
+      $has_reason = $this->hasContent($type);
+      if ($has_reason) {
+        $has_reason = $type;
+        break;
+      }
+    }
+    if ($has_reason) {
+      return $has_reason;
+    }
+    foreach ($media_types as $type) {
+      $has_reason = $this->hasMedia($type);
+      if ($has_reason) {
+        $has_reason = $type;
+        break;
+      }
+    }
+    return $has_reason;
   }
 
   /**
