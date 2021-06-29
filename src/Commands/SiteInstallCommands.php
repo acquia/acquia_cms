0d6d4d419bba4175df41eac31fe8a36bd13ecea8
Panshulk <panshul.khurana@acquia.com>
2021-06-29 17:03:21 +0530

    ACMS-768: Added code for logging time for install rebuild start & end. (#841)

--- src/Commands/SiteInstallCommands.php
+++ src/Commands/SiteInstallCommands.php
@@ -4,6 +4,7 @@ namespace Drush\Commands;
 
 use Consolidation\AnnotatedCommand\CommandData;
 use Consolidation\AnnotatedCommand\CommandResult;
+use Drupal\Core\Datetime\DrupalDateTime;
 
 /**
  * Rebuild site studio when site is installed via drush.
@@ -19,11 +20,28 @@ class SiteInstallCommands extends DrushCommands {
     $arguments = $commandData->arguments();
     $moduleHandler = \Drupal::service('module_handler');
     if (isset($arguments['profile'][0]) && $arguments['profile'][0] == 'acquia_cms' && $moduleHandler->moduleExists('cohesion')) {
+      $rebuild_start_time = new DrupalDateTime();
+      $formatted = \Drupal::service('date.formatter')->format(
+        $rebuild_start_time->getTimestamp(), 'custom', 'Y-m-d h:i:s'
+      );
+      \Drupal::state()->set('rebuild_start_time', $formatted);
       $this->say(dt('Rebuilding all entities.'));
       $result = \Drupal::service('acquia_cms_common.utility')->rebuildSiteStudio();
       $this->yell('Finished rebuilding.');
+      $this->setFinishedTime();
       return is_array($result) && isset(array_shift($result)['error']) ? CommandResult::exitCode(self::EXIT_FAILURE) : CommandResult::exitCode(self::EXIT_SUCCESS);
     }
+  }
+
+  /**
+   * Set site studio rebuild time once rebuild has completed.
+   */
+  public function setFinishedTime() {
+    $rebuild_end_time = new DrupalDateTime();
+    $formatted = \Drupal::service('date.formatter')->format(
+      $rebuild_end_time->getTimestamp(), 'custom', 'Y-m-d h:i:s'
+    );
+    \Drupal::state()->set('rebuild_end_time', $formatted);
   }
 
 }
