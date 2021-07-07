83497f792c13d4dbeb2994a6874fcc722763048f
Vishal Khode <81069167+vishalkhode1@users.noreply.github.com>
2021-07-07 19:31:59 +0530

    ACMS-740: Allow acquia_cms_site_studio module to be install with custom profile. (#854)

--- modules/acquia_cms_site_studio/src/Form/AcquiaCmsSiteStudioSiteConfigureForm.php
+++ modules/acquia_cms_site_studio/src/Form/AcquiaCmsSiteStudioSiteConfigureForm.php
@@ -2,46 +2,41 @@
 
 namespace Drupal\acquia_cms_site_studio\Form;
 
-use Drupal\acquia_cms\Form\SiteConfigureForm;
-use Drupal\acquia_cms_tour\Form\AcquiaGoogleMapsAPIForm;
 use Drupal\Core\Config\ConfigFactoryInterface;
-use Drupal\Core\Extension\ModuleHandlerInterface;
-use Drupal\Core\Extension\ModuleInstallerInterface;
+use Drupal\Core\Form\ConfigFormBase;
 use Drupal\Core\Form\FormStateInterface;
-use Drupal\Core\Installer\Form\SiteConfigureForm as CoreSiteConfigureForm;
 use Symfony\Component\DependencyInjection\ContainerInterface;
 
 /**
  * Extends the installer's site configuration form to configure Cohesion.
  */
-class AcquiaCmsSiteStudioSiteConfigureForm extends SiteConfigureForm {
+class AcquiaCmsSiteStudioSiteConfigureForm extends ConfigFormBase {
 
   /**
    * The Cohesion API URL.
    *
    * @var string
    */
-  private $apiUrl;
+  protected $apiUrl;
 
   /**
-   * SiteConfigureForm constructor.
+   * Constructs a \Drupal\system\ConfigFormBase object.
    *
    * @param \Drupal\Core\Config\ConfigFactoryInterface $config_factory
-   *   The config factory service.
-   * @param string $api_url
-   *   The Cohesion API URL.
-   * @param \Drupal\Core\Extension\ModuleInstallerInterface $module_installer
-   *   The module installer.
-   * @param \Drupal\Core\Extension\ModuleHandlerInterface $module_handler
-   *   The module handler.
-   * @param \Drupal\Core\Installer\Form\SiteConfigureForm $site_form
-   *   The decorated site configuration form object.
-   * @param \Drupal\acquia_cms_tour\Form\AcquiaGoogleMapsAPIForm $maps_form
-   *   The decorated Google Maps configuration form object.
+   *   The factory for configuration objects.
+   * @param string $apiUrl
+   *   The Site Studio api url.
    */
-  public function __construct(ConfigFactoryInterface $config_factory, string $api_url, ModuleInstallerInterface $module_installer, ModuleHandlerInterface $module_handler, CoreSiteConfigureForm $site_form, AcquiaGoogleMapsAPIForm $maps_form) {
-    parent::__construct($config_factory, $module_installer, $module_handler, $site_form, $maps_form);
-    $this->apiUrl = $api_url;
+  public function __construct(ConfigFactoryInterface $config_factory, string $apiUrl) {
+    parent::__construct($config_factory);
+    $this->apiUrl = $apiUrl;
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function getFormId() {
+    return 'acquia_cms_site_studio_site_installer_form';
   }
 
   /**
@@ -50,11 +45,7 @@ class AcquiaCmsSiteStudioSiteConfigureForm extends SiteConfigureForm {
   public static function create(ContainerInterface $container) {
     return new static(
       $container->get('config.factory'),
-      $container->get('cohesion.api.utils')->getAPIServerURL(),
-      $container->get('module_installer'),
-      $container->get('module_handler'),
-      CoreSiteConfigureForm::create($container),
-      AcquiaGoogleMapsAPIForm::create($container)
+      $container->get('cohesion.api.utils')->getAPIServerURL()
     );
   }
 
@@ -69,7 +60,6 @@ class AcquiaCmsSiteStudioSiteConfigureForm extends SiteConfigureForm {
    * {@inheritdoc}
    */
   public function buildForm(array $form, FormStateInterface $form_state) {
-    $form = parent::buildForm($form, $form_state);
     $form['cohesion'] = [
       'api_key' => [
         '#type' => 'textfield',
@@ -93,7 +83,6 @@ class AcquiaCmsSiteStudioSiteConfigureForm extends SiteConfigureForm {
    * {@inheritdoc}
    */
   public function submitForm(array &$form, FormStateInterface $form_state) {
-
     $api_key = $form_state->getValue(['cohesion', 'api_key']);
     $org_key = $form_state->getValue(['cohesion', 'organization_key']);
 
@@ -111,7 +100,6 @@ class AcquiaCmsSiteStudioSiteConfigureForm extends SiteConfigureForm {
         ->set('organization_key', $org_key)
         ->save(TRUE);
     }
-    parent::submitForm($form, $form_state);
   }
 
 }
