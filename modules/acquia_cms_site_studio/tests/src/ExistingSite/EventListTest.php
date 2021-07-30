f4d6e6738d4144f837dbb5fa2e3db7156598455d
Chandan Singh <chandan.singh@acquia.com>
2021-07-30 11:57:53 +0530

    ACMS-827: Move content ExistingSite test to acquia_cms_site_studio module

--- modules/acquia_cms_site_studio/tests/src/ExistingSite/EventListTest.php
+++ modules/acquia_cms_site_studio/tests/src/ExistingSite/EventListTest.php
@@ -1,6 +1,6 @@
 <?php
 
-namespace Drupal\Tests\acquia_cms_search\ExistingSite;
+namespace Drupal\Tests\acquia_cms_site_studio\ExistingSite;
 
 use Drupal\Core\Entity\Query\QueryInterface;
 use Drupal\node\Entity\Node;
@@ -11,7 +11,7 @@ use Drupal\views\Entity\View;
  * Tests the "all events" listing page.
  *
  * @group acquia_cms
- * @group acquia_cms_search
+ * @group acquia_cms_site_studio
  * @group low_risk
  * @group pr
  * @group push
