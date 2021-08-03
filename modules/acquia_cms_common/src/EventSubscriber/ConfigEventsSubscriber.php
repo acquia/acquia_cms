87f77bdf72446a6159c85747ca8374b914457d6d
Chandan Singh <chandan.singh@acquia.com>
2021-08-03 10:56:10 +0530

    ACMS-827: Update code to use dependency injection

--- modules/acquia_cms_common/src/EventSubscriber/ConfigEventsSubscriber.php
+++ modules/acquia_cms_common/src/EventSubscriber/ConfigEventsSubscriber.php
@@ -5,14 +5,43 @@ namespace Drupal\acquia_cms_common\EventSubscriber;
 use Drupal\acquia_cms_search\Facade\SearchFacade;
 use Drupal\Core\Config\ConfigCrudEvent;
 use Drupal\Core\Config\ConfigEvents;
+use Drupal\Core\DependencyInjection\ClassResolverInterface;
+use Drupal\Core\Extension\ModuleHandlerInterface;
 use Symfony\Component\EventDispatcher\EventSubscriberInterface;
 
 /**
- * Class EntityTypeSubscriber.
+ * Update views configuration based on modules' availability.
  *
  * @package Drupal\acquia_cms_common\EventSubscriber
  */
 class ConfigEventsSubscriber implements EventSubscriberInterface {
+
+  /**
+   * The module handler.
+   *
+   * @var \Drupal\Core\Extension\ModuleHandlerInterface
+   */
+  protected $moduleHandler;
+
+  /**
+   * The ClassResolver.
+   *
+   * @var \Drupal\Core\DependencyInjection\ClassResolver
+   */
+  protected $searchFacade;
+
+  /**
+   * Constructs a new ConfigEventsSubscriber object.
+   *
+   * @param \Drupal\Core\Extension\ModuleHandlerInterface $module_handler
+   *   The ModuleHandlerInterface.
+   * @param \Drupal\Core\DependencyInjection\ClassResolverInterface $class_resolver
+   *   The class resolver.
+   */
+  public function __construct(ModuleHandlerInterface $module_handler, ClassResolverInterface $class_resolver) {
+    $this->moduleHandler = $module_handler;
+    $this->searchFacade = $class_resolver->getInstanceFromDefinition(SearchFacade::class);
+  }
 
   /**
    * {@inheritdoc}
@@ -34,61 +63,60 @@ class ConfigEventsSubscriber implements EventSubscriberInterface {
    */
   public function configSave(ConfigCrudEvent $event) {
     $config = $event->getConfig();
-    $module_handler = \Drupal::moduleHandler();
     // Update views display_options's style type as cohesion_layout and
     // option's views_template & master_template if acquia_cms_site_studio
     // module is present else use default one.
-    if ($module_handler->moduleExists('acquia_cms_site_studio')) {
+    if ($this->moduleHandler->moduleExists('acquia_cms_site_studio')) {
       switch ($config->getName()) {
         case 'views.view.event_cards':
-          \Drupal::classResolver(SearchFacade::class)->updateViewDisplayOptionsStyle('event_cards', 'upcoming_events_block', 'view_tpl_event_cards_slider');
+          $this->searchFacade->updateViewDisplayOptionsStyle('event_cards', 'upcoming_events_block', 'view_tpl_event_cards_slider');
           break;
 
         case 'views.view.article_cards':
-          \Drupal::classResolver(SearchFacade::class)->updateViewDisplayOptionsStyle('article_cards', 'recent_articles_block', 'view_tpl_article_cards_slider');
+          $this->searchFacade->updateViewDisplayOptionsStyle('article_cards', 'recent_articles_block', 'view_tpl_article_cards_slider');
           break;
       }
     }
-    if ($module_handler->moduleExists('acquia_cms_site_studio') && $module_handler->moduleExists('acquia_cms_search')) {
+    if ($this->moduleHandler->moduleExists('acquia_cms_site_studio') && $this->moduleHandler->moduleExists('acquia_cms_search')) {
       switch ($config->getName()) {
         case 'views.view.articles':
-          \Drupal::classResolver(SearchFacade::class)->updateViewDisplayOptionsStyle('articles');
+          $this->searchFacade->updateViewDisplayOptionsStyle('articles');
           break;
 
         case 'views.view.articles_fallback':
-          \Drupal::classResolver(SearchFacade::class)->updateViewDisplayOptionsStyle('articles_fallback');
+          $this->searchFacade->updateViewDisplayOptionsStyle('articles_fallback');
           break;
 
         case 'views.view.events':
-          \Drupal::classResolver(SearchFacade::class)->updateViewDisplayOptionsStyle('events');
+          $this->searchFacade->updateViewDisplayOptionsStyle('events');
           break;
 
         case 'views.view.events_fallback':
-          \Drupal::classResolver(SearchFacade::class)->updateViewDisplayOptionsStyle('events_fallback');
+          $this->searchFacade->updateViewDisplayOptionsStyle('events_fallback');
           break;
 
         case 'views.view.places':
-          \Drupal::classResolver(SearchFacade::class)->updateViewDisplayOptionsStyle('places');
+          $this->searchFacade->updateViewDisplayOptionsStyle('places');
           break;
 
         case 'views.view.places_fallback':
-          \Drupal::classResolver(SearchFacade::class)->updateViewDisplayOptionsStyle('places_fallback');
+          $this->searchFacade->updateViewDisplayOptionsStyle('places_fallback');
           break;
 
         case 'views.view.people':
-          \Drupal::classResolver(SearchFacade::class)->updateViewDisplayOptionsStyle('people');
+          $this->searchFacade->updateViewDisplayOptionsStyle('people');
           break;
 
         case 'views.view.people_fallback':
-          \Drupal::classResolver(SearchFacade::class)->updateViewDisplayOptionsStyle('people_fallback');
+          $this->searchFacade->updateViewDisplayOptionsStyle('people_fallback');
           break;
 
         case 'views.view.search':
-          \Drupal::classResolver(SearchFacade::class)->updateViewDisplayOptionsStyle('search');
+          $this->searchFacade->updateViewDisplayOptionsStyle('search');
           break;
 
         case 'views.view.search_fallback':
-          \Drupal::classResolver(SearchFacade::class)->updateViewDisplayOptionsStyle('search_fallback');
+          $this->searchFacade->updateViewDisplayOptionsStyle('search_fallback');
           break;
       }
     }
