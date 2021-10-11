50a82f7a8b3c55119b4929d191613a64a5af158e
Vishal Khode <vishal.khode@acquia.com>
2021-10-12 00:04:05 +0530

    ACMS-934: Include CI settings.php to provide default db infomation.

---
+++ docroot/sites/default/ci.settings.php
@@ -0,0 +1,12 @@
+<?php
+
+$databases['default']['default'] = array (
+  'database' => 'drupal',
+  'username' => 'drupal',
+  'password' => 'drupal',
+  'prefix' => '',
+  'host' => '127.0.0.1',
+  'port' => '3306',
+  'namespace' => 'Drupal\\Driver\\Database\\mysql',
+  'driver' => 'mysql',
+);
