2ea60c9f5cebd42dfbf897c2297504f74e97c286
Katherine Druckman <katherine.druckman@acquia.com>
2021-10-21 13:26:57 -0500

    ACMS-946: Add comments and interactivity for ease of use.

--- create-tarball.sh
+++ create-tarball.sh
@@ -1,8 +1,13 @@
 #!/bin/bash
 
-ARCHIVE=acms-$1
+# Usage run create-tarball.sh and enter the current version to generate a tarball.
 
-composer create-project --stability beta --no-install drupal/legacy-project:~9.1.0 $ARCHIVE
+read -p "Enter the ACMS version (ex: 1.3.0): " VERSION
+ARCHIVE=acms-${VERSION}
+
+# Creates a project using the legacy drupal project layout with the vendor folder
+# inside the root directory.
+composer create-project --stability beta --no-install drupal/legacy-project:~9.2.0 $ARCHIVE
 composer dump-autoload
 composer configure-tarball $ARCHIVE
 
@@ -12,11 +17,11 @@ composer config prefer-stable true
 composer config repositories.assets composer https://asset-packagist.org
 
 composer config repositories.acms vcs git@github.com:acquia/acquia_cms.git
-composer require --no-update "ext-dom:*" "acquia/acquia_cms:$1" cweagans/composer-patches
+composer require --no-update "ext-dom:*" "acquia/acquia_cms:${VERSION}" cweagans/composer-patches
 composer update
 
 # Add the version number to the info file.
-echo "version: $1" >> ./profiles/contrib/acquia_cms/acquia_cms.info.yml
+echo "version: ${VERSION}" >> ./profiles/contrib/acquia_cms/acquia_cms.info.yml
 
 # Wrap it all up in a nice compressed tarball.
 cd ..
