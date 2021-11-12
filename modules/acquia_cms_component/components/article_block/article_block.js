dede4ba4f0ab373428d28f260caffa111fa6b4a5
Chandan Singh <chandan.singh@acquia.com>
2021-11-12 12:25:14 +0530

    ACMS-930: Implement using reactjs.

--- modules/acquia_cms_component/components/article_block/article_block.js
+++ modules/acquia_cms_component/components/article_block/article_block.js
@@ -1,41 +1,47 @@
+const Articles = createReactClass({
+  getInitialState: function() {
+    this.state = {
+      data: [],
+      loaded: false,
+    };
+    return({message: null});
+  },
+  componentDidMount() {
+    // Map select option with component configuration form select.
+    const articleBlock = document.getElementById('article-block-component').parentElement;
+    const selectOption = ['5', '10', '20', '30', '50', 'All'];
+    const dataDisplayItem = articleBlock.getAttribute('data-display-item');
+    const itemToDisplay = selectOption[dataDisplayItem];
+    let endpoint = '/jsonapi/node/article?sort=-created';
+    if (itemToDisplay && itemToDisplay !=='All') {
+      endpoint = '/jsonapi/node/article?page[offset]=0&page[limit]=' + itemToDisplay;
+    }
+    fetch(endpoint)
+      .then(response => response.json())
+      .then(data => this.setState({ data: data.data, loaded: true }));
+  },
+  render() {
+    const { data, loaded } = this.state;
+    if (!loaded) {
+      return <div className="loader" />;
+    }
+    if (data.length === 0) {
+      return <p>No results</p>;
+    }
+    return (
+      <div>
+        <ul>
+          {data.map(article =>
+            <li>
+              <a href={article.attributes.path.alias}>{article.attributes.title}</a>
+            </li>
+          )}
+        </ul>
+      </div>
+    );
+  }
+});
 
-/**
- * @file
- * Contains article block component code.
- */
-
-(function ($) {
-  Drupal.behaviors.articleBlockComponent = {
-    attach: function(context, settings) {
-      $('#article-block-component', context).once('article-block-component').each(function() {
-        // Add loader span.
-        $('#article-block-component').html('<div class="loader"></div>');
-        // Map select option with component configuration form select.
-        let selectOption = ['5', '10', '20', '30', '50', 'All'];
-        let data_display_item = $('.article_block').attr('data-display-item');
-        let item_to_display = selectOption[data_display_item];
-        let endpoint = '/jsonapi/node/article';
-        if (item_to_display && item_to_display !=='All') {
-          endpoint = '/jsonapi/node/article?page[offset]=0&page[limit]=' + item_to_display;
-        }
-        $.get( endpoint, function( response ) {
-          let data = response.data;
-          if (data.length > 0) {
-            let ul = $('<ul/>');
-            $.each(data, function (index, value) {
-              let li = $('<li/>');
-              let anchor = $('<a/>');
-              anchor.attr('href', value.attributes.path.alias);
-              anchor.text(value.attributes.title);
-              li.append(anchor);
-              ul.append(li);
-            });
-            $('#article-block-component').html(ul);
-          }
-        });
-      }).fail(function () {
-        $('#article-block-component').html('<span>Something went wrong!</span>');
-      });
-    },
-  };
-})(jQuery);
+ReactDOM.render(<Articles />,
+  document.getElementById('article-block-component')
+);
