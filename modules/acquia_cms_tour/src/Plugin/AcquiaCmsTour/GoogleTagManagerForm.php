acf624c9ef89f9f2f03803a92f87320e6f9ac173
Chandan Singh <chandan.singh@acquia.com>
2022-03-04 18:23:44 +0530

    ACMS-1078: Create acms tour plugin for each form.

---
+++ modules/acquia_cms_tour/src/Plugin/AcquiaCmsTour/GoogleTagManagerForm.php
@@ -0,0 +1,133 @@
+<?php
+
+namespace Drupal\acquia_cms_tour\Plugin\AcquiaCmsTour;
+
+use Drupal\acquia_cms_tour\Form\AcquiaCMSDashboardBase;
+use Drupal\Core\Form\FormStateInterface;
+use Drupal\Core\Url;
+
+/**
+ * Plugin implementation of the acquia_cms_tour.
+ *
+ * @AcquiaCmsTour(
+ *   id = "google_tag",
+ *   label = @Translation("Google TagManager"),
+ *   weight = 5
+ * )
+ */
+class GoogleTagManagerForm extends AcquiaCMSDashboardBase {
+
+  /**
+   * Provides module name.
+   *
+   * @var string
+   */
+  protected $module = 'google_tag';
+
+  /**
+   * {@inheritdoc}
+   */
+  public function getFormId() {
+    return 'acquia_cms_google_tag_manager_form';
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  protected function getEditableConfigNames() {
+    return [
+      'google_tag.settings',
+    ];
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function buildForm(array $form, FormStateInterface $form_state) {
+    $form['#tree'] = FALSE;
+    $module = $this->module;
+    if ($this->isModuleEnabled()) {
+      $configured = $this->getConfigurationState();
+      if ($configured) {
+        $form['check_icon'] = [
+          '#prefix' => '<span class= "dashboard-check-icon">',
+          '#suffix' => "</span>",
+        ];
+      }
+      $module_path = $this->module_handler->getModule($module)->getPathname();
+      $module_info = $this->infoParser->parse($module_path);
+      $form[$module] = [
+        '#type' => 'details',
+        '#title' => $module_info['name'],
+        '#collapsible' => TRUE,
+        '#collapsed' => TRUE,
+      ];
+      $form[$module]['snippet_parent_uri'] = [
+        '#type' => 'textfield',
+        '#required' => TRUE,
+        '#title' => $this->t('Snippet parent URI'),
+        '#attributes' => ['placeholder' => $this->t('public:/')],
+        '#default_value' => $this->config('google_tag.settings')->get('uri'),
+        '#prefix' => '<div class= "dashboard-fields-wrapper">' . $module_info['description'],
+        '#suffix' => "</div>",
+      ];
+      $form[$module]['actions']['submit'] = [
+        '#type' => 'submit',
+        '#value' => 'Save',
+        '#prefix' => '<div class= "dashboard-buttons-wrapper">',
+      ];
+      $form[$module]['actions']['ignore'] = [
+        '#type' => 'submit',
+        '#value' => 'Ignore',
+        '#limit_validation_errors' => [],
+        '#submit' => ['::ignoreConfig'],
+      ];
+      if (isset($module_info['configure'])) {
+        $form[$module]['actions']['advanced'] = [
+          '#prefix' => '<div class= "dashboard-tooltiptext">',
+          '#markup' => $this->linkGenerator->generate(
+            'Advanced',
+            Url::fromRoute($module_info['configure'])
+          ),
+          '#suffix' => "</div>",
+        ];
+        $form[$module]['actions']['advanced']['information'] = [
+          '#prefix' => '<b class= "tool-tip__icon">i',
+          '#suffix' => "</b>",
+        ];
+        $form[$module]['actions']['advanced']['tooltip-text'] = [
+          '#prefix' => '<span class= "tooltip">',
+          '#markup' => $this->t("Opens Advance Configuration in new tab"),
+          '#suffix' => "</span></div>",
+        ];
+      }
+    }
+    return $form;
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function submitForm(array &$form, FormStateInterface $form_state) {
+    $snippet_parent_uri = $form_state->getValue(['snippet_parent_uri']);
+    $this->config('google_tag.settings')->set('uri', $snippet_parent_uri)->save();
+    $this->setConfigurationState();
+    $this->messenger()->addStatus('The configuration options have been saved.');
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function ignoreConfig(array &$form, FormStateInterface $form_state) {
+    $this->setConfigurationState();
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function checkMinConfiguration(): bool {
+    $uri = $this->config('google_tag.settings')->get('uri');
+    return (bool) $uri;
+  }
+
+}
