cb0d7bf2b8a7bbec3d867919f3a5bb8b7bd6e8c5
Rajeshreeputra <pravin.gaikwad@acquia.com>
2022-03-21 19:48:09 +0530

    ACMS-1099: updated article template to make content type reference fields optional

--- modules/acquia_cms_article/tests/src/Functional/ArticleTest.php
+++ modules/acquia_cms_article/tests/src/Functional/ArticleTest.php
@@ -148,8 +148,7 @@ class ArticleTest extends ContentTypeTestBase {
     $page->pressButton('Save');
     $assert_session->pageTextContains('Title field is required.');
     $assert_session->pageTextContains('Body field is required.');
-    //$assert_session->pageTextContains('Display Author field is required.');
-
+    // $assert_session->pageTextContains('Display Author field is required.');
     // Fill in the required fields and assert that things went as expected.
     $page->fillField('Title', 'Local news');
     $page->fillField('Body', 'This is an example of body text');
