d1c84e54415b95de31c4e24aac31efa953ca05c5
Wade Stewart <themodularlab@3190381.no-reply.drupal.org>
2022-03-29 15:13:03 -0600

    ACMS-1098: [WIP] Initial API setup wizard.

---
+++ modules/acquia_cms_headless/src/Plugin/AcquiaCmsTour/AcquiaHeadlessForm.php
@@ -0,0 +1,147 @@
+<?php
+
+namespace Drupal\acquia_cms_headless\Plugin\AcquiaCmsTour;
+
+use Drupal\acquia_cms_tour\Form\AcquiaCMSDashboardBase;
+use Drupal\Core\Form\FormStateInterface;
+use Drupal\Core\Url;
+
+/**
+ * Plugin implementation of the acquia_cms_tour.
+ *
+ * @AcquiaCmsTour(
+ *   id = "acquia_cms_headless",
+ *   label = @Translation("Acquia CMS Headless"),
+ *   weight = 8
+ * )
+ */
+class AcquiaHeadlessForm extends AcquiaCMSDashboardBase {
+
+  /**
+   * Provides module name.
+   *
+   * @var string
+   */
+  protected $module = 'acquia_cms_headless';
+
+  /**
+   * {@inheritdoc}
+   */
+  public function getFormId() {
+    return 'acquia_cms_headless_form';
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  protected function getEditableConfigNames() {
+    return ['acquia_cms_headless.settings'];
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function buildForm(array $form, FormStateInterface $form_state) {
+    $form['#tree'] = FALSE;
+    $module = $this->module;
+    $config = $this->config('acquia_cms_headless.settings');
+
+    if ($this->isModuleEnabled()) {
+
+      $configured = $this->getConfigurationState();
+      if ($configured) {
+        $form['check_icon'] = [
+          '#prefix' => '<span class= "dashboard-check-icon">',
+          '#suffix' => "</span>",
+        ];
+      }
+      $form[$module] = [
+        '#type' => 'details',
+        '#title' => $this->t('Headless'),
+        '#collapsible' => TRUE,
+        '#collapsed' => TRUE,
+      ];
+      $form[$module]['robust_api'] = [
+        '#type' => 'checkbox',
+        '#required' => FALSE,
+        '#title' => $this->t('Enable robust API capabilities (Next.js Drupal)'),
+        '#description' => $this->t('@todo Input description of what happens when Robust API is enabled.'),
+        '#default_value' => $config->get('robust_api'),
+        '#prefix' => '<div class= "dashboard-fields-wrapper">' . $this->t('@todo Provide better description of these options, etc.'),
+      ];
+      $form[$module]['headless_mode'] = [
+        '#type' => 'checkbox',
+        '#required' => FALSE,
+        '#title' => $this->t('Enable "Pure Headless" mode'),
+        '#description' => $this->t('@todo Input description of what happens when Pure Headless mode is enabled.'),
+        '#default_value' => $config->get('headless_mode'),
+        '#suffix' => "</div>",
+      ];
+      $form[$module]['actions']['submit'] = [
+        '#type' => 'submit',
+        '#value' => 'Save',
+        '#button_type' => 'primary',
+        '#prefix' => '<div class= "dashboard-buttons-wrapper">',
+      ];
+      $form[$module]['actions']['ignore'] = [
+        '#type' => 'submit',
+        '#value' => 'Ignore',
+        '#limit_validation_errors' => [],
+        '#submit' => ['::ignoreConfig'],
+      ];
+      if (isset($module_info['configure'])) {
+        // @todo Link to API dashboard. Will be added via AMCS-1083.
+        $form[$module]['actions']['advanced'] = [
+          '#prefix' => '<div class= "dashboard-tooltiptext">',
+          '#markup' => $this->linkGenerator->generate(
+            'Advanced',
+            Url::fromRoute('cohesion.configuration.account_settings')
+          ),
+          '#suffix' => "</div>",
+        ];
+        $form[$module]['actions']['advanced']['information'] = [
+          '#prefix' => '<b class= "tool-tip__icon">i',
+          '#suffix' => "</b>",
+        ];
+        $form[$module]['actions']['advanced']['tooltip-text'] = [
+          '#prefix' => '<span class= "tooltip">',
+          '#markup' => $this->t("Opens Advance Configuration in new tab"),
+          '#suffix' => "</span></div>",
+        ];
+      }
+
+      return $form;
+    }
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function submitForm(array &$form, FormStateInterface $form_state) {
+    $acms_robust_api = $form_state->getValue(['robust_api']);
+    $acms_headless_mode = $form_state->getValue(['headless_mode']);
+    if ($acms_robust_api && $acms_headless_mode) {
+      $this->config('acquia_cms_headless.settings')->set('robust_api', $acms_robust_api)->save();
+      $this->config('acquia_cms_headless.settings')->set('headless_mode', $acms_headless_mode)->save();
+    }
+    $this->setConfigurationState();
+    $this->messenger()->addStatus('The configuration options have been saved.');
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function ignoreConfig(array &$form, FormStateInterface $form_state) {
+    $this->setConfigurationState();
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function checkMinConfiguration(): bool {
+    $robust_api = (bool) $this->config('acquia_cms_headless.settings')->get('robust_api');
+    $headless_mode = (bool) $this->config('acquia_cms_headless.settings')->get('headless_mode');
+    return $robust_api && $headless_mode;
+  }
+
+}
