bb8e1a65e3060fbf8ba3c18a0ec2decf37d42782
Wade Stewart <themodularlab@3190381.no-reply.drupal.org>
2022-04-16 14:47:48 -0600

    ACMS-1083: Adds Next JS Sites API plugin.

---
+++ modules/acquia_cms_headless/src/Plugin/AcquiaCmsHeadless/AcquiaHeadlessNextSites.php
@@ -0,0 +1,235 @@
+<?php
+
+namespace Drupal\acquia_cms_headless\Plugin\AcquiaCmsHeadless;
+
+use Drupal\acquia_cms_tour\Form\AcquiaCMSDashboardBase;
+use Drupal\Core\Database\Connection;
+use Drupal\Core\Entity\EntityTypeManagerInterface;
+use Drupal\Core\Extension\InfoParserInterface;
+use Drupal\Core\Extension\ModuleHandlerInterface;
+use Drupal\Core\Form\FormStateInterface;
+use Drupal\Core\State\StateInterface;
+use Drupal\Core\Utility\LinkGeneratorInterface;
+use Symfony\Component\DependencyInjection\ContainerInterface;
+
+/**
+ * Plugin implementation of the acquia_cms_tour.
+ *
+ * @AcquiaCmsHeadless(
+ *   id = "headless_api_url",
+ *   label = @Translation("Acquia CMS Headless API URL"),
+ *   weight = 8
+ * )
+ */
+class AcquiaHeadlessNextSites extends AcquiaCMSDashboardBase {
+  /**
+   * The state interface.
+   *
+   * @var \Drupal\Core\State\StateInterface
+   */
+  protected $state;
+
+
+  /**
+   * The module handler.
+   *
+   * @var \Drupal\Core\Extension\ModuleHandlerInterface
+   */
+  protected $moduleHandler;
+
+  /**
+   * The link generator.
+   *
+   * @var \Drupal\Core\Utility\LinkGeneratorInterface
+   */
+  protected $linkGenerator;
+
+  /**
+   * The info file parser.
+   *
+   * @var \Drupal\Core\Extension\InfoParserInterface
+   */
+  protected $infoParser;
+
+  /**
+   * Provides the database connection.
+   *
+   * @var \Drupal\Core\Database\Connection
+   */
+  protected $connection;
+
+  /**
+   * The EntityTypeManager service.
+   *
+   * @var \Drupal\Core\Entity\EntityTypeManagerInterface
+   */
+  protected $entityTypeManager;
+
+  /**
+   * Provides module name.
+   *
+   * @var string
+   */
+  protected $module = 'next';
+
+  /**
+   * {@inheritdoc}
+   */
+  public function __construct(StateInterface $state, ModuleHandlerInterface $module_handler, LinkGeneratorInterface $link_generator, InfoParserInterface $info_parser, Connection $connection, EntityTypeManagerInterface $entity_type_manager) {
+    parent::__construct($state, $module_handler, $link_generator, $info_parser);
+    $this->connection = $connection;
+    $this->entityTypeManager = $entity_type_manager;
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public static function create(ContainerInterface $container) {
+    return new static(
+      $container->get('state'),
+      $container->get('module_handler'),
+      $container->get('link_generator'),
+      $container->get('info_parser'),
+      $container->get('database'),
+      $container->get('entity_type.manager')
+    );
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function getFormId() {
+    return 'acquia_cms_headless_next_sites';
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  protected function getEditableConfigNames() {
+    return ['acquia_cms_headless.settings'];
+  }
+
+  /**
+   * Gets Entity data.
+   *
+   * @return array|int
+   *   Returns an array of entity ids.
+   *
+   * @throws \Drupal\Component\Plugin\Exception\InvalidPluginDefinitionException
+   * @throws \Drupal\Component\Plugin\Exception\PluginNotFoundException
+   */
+  public function getEntityData() {
+    $header = $this->buildEntityHeader();
+    $storage = $this->entityTypeManager->getStorage('next_site');
+    $query = $storage->getQuery();
+    $query->tableSort($header);
+    $query->pager(2);
+
+    return $query->execute();
+  }
+
+  /**
+   * Build the form table header.
+   *
+   * @return array
+   *   Returns an array with table header data.
+   */
+  public function buildEntityHeader(): array {
+    return [
+      'id' => [
+        'data' => $this->t('ID'),
+        'specifier' => 'id',
+        'field' => 't.alpha',
+        'sort' => 'asc',
+      ],
+      'label' => [
+        'data' => $this->t('Name'),
+        'specifier' => 'label',
+      ],
+      'base_url' => [
+        'data' => $this->t('Site URL'),
+        'specifier' => 'base_url',
+      ],
+    ];
+  }
+
+  /**
+   * Builds rows for the Form Table.
+   *
+   * @return array
+   *   Returns an array of rows.
+   *
+   * @throws \Drupal\Component\Plugin\Exception\InvalidPluginDefinitionException
+   * @throws \Drupal\Component\Plugin\Exception\PluginNotFoundException
+   * @throws \Drupal\Core\TypedData\Exception\MissingDataException
+   */
+  public function buildEntityRows(): array {
+    $rows = [];
+    $next_sites = $this->getEntityData();
+    $storage = $this->entityTypeManager->getStorage('next_site');
+    $sites = $storage->loadMultiple($next_sites);
+
+    foreach ($sites as $site) {
+      $row = [
+        'id' => $site->id(),
+        'label' => $site->label(),
+        'base_url' => $site->getTypedData()->get('base_url')->getValue(),
+      ];
+
+      $rows[$site->uuid()] = $row;
+    }
+
+    return $rows;
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function buildForm(array $form, FormStateInterface $form_state): array {
+    $form['#tree'] = FALSE;
+    $module = $this->module;
+    $header = $this->buildEntityHeader();
+    $rows = $this->buildEntityRows();
+
+    $form[$module] = [
+      '#type' => 'fieldset',
+      '#title' => $this->t('API URL'),
+    ];
+
+    $form[$module]['table'] = [
+      '#type' => 'table',
+      '#header' => $header,
+      '#rows' => $rows,
+      '#empty' => $this->t('No users found'),
+    ];
+
+    $form[$module]['pager'] = [
+      '#type' => 'pager',
+    ];
+
+    return $form;
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function submitForm(array &$form, FormStateInterface $form_state) {
+    // Set the config state.
+    $this->setConfigurationState();
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function ignoreConfig(array &$form, FormStateInterface $form_state) {
+    $this->setConfigurationState();
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function checkMinConfiguration(): bool {
+    return TRUE;
+  }
+
+}
