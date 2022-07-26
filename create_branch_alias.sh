48a5e8cdb42ecc0a491d07e95e5dd4a11bc7c176
Vishal Khode <vishal.khode@acquia.com>
2022-07-26 14:33:05 +0530

    ACMS-000: develop.

--- create_branch_alias.sh
+++ create_branch_alias.sh
@@ -6,20 +6,23 @@ LATEST_TAG=$(git describe --abbrev=0 --tags | head -n 1);
   # Remove the -alpha-N or -beta-N or -rc-N etc. where N is number.
   NEW_TAG="$(echo $NEW_TAG | sed 's/\-.*//')"
   CURRENT_BRANCH=$(git rev-parse --abbrev-ref HEAD)
+  echo "The current Branch is: $CURRENT_BRANCH"
   if [ "$CURRENT_BRANCH" = "HEAD" ]; then
     MODULE=$(pwd | sed 's/.*\///')
     if [ "$MODULE" = "acquia_cms_common" ]; then
-      git fetch origin develop-1.4.x
-      git checkout develop-1.4.x
+      #git fetch origin develop-1.4.x
+      git checkout -b develop-1.4.x
     elif [ "$MODULE" = "acquia_cms_tour" ]; then
-        git fetch origin develop-2.0.x
-        git checkout develop-2.0.x
+       # git fetch origin develop-2.0.x
+        git checkout -b develop-2.0.x
     else
-      git fetch origin develop
-      git checkout develop
+      #git fetch origin develop
+      git checkout -b develop
     fi
+    CURRENT_BRANCH=$(git rev-parse --abbrev-ref HEAD)
   fi
   echo "$NEW_TAG, dev-$CURRENT_BRANCH"
   ALIAS="{\"dev-$CURRENT_BRANCH\": \"$NEW_TAG-dev\"}"
+  echo "The Alias is: $ALIAS"
   composer config extra.branch-alias "$ALIAS" --json
 #fi;
