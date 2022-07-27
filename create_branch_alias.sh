62d6d9666c34d4d4460f4eec599432a2b8e18151
Vishal Khode <vishal.khode@acquia.com>
2022-07-27 12:23:07 +0530

    ACMS-000: Fixed branch alias script.

--- create_branch_alias.sh
+++ create_branch_alias.sh
@@ -4,18 +4,29 @@ LATEST_TAG=$(git describe --abbrev=0 --tags | head -n 1);
   NEW_TAG="$LATEST_TAG"
 
   # Remove the -alpha-N or -beta-N or -rc-N etc. where N is number.
-  NEW_TAG="$(echo $NEW_TAG | sed 's/\-.*//')"
+  #NEW_TAG="$(echo $NEW_TAG | sed 's/\-.*//')"
   CURRENT_BRANCH=$(git rev-parse --abbrev-ref HEAD)
+  #echo $CURRENT_BRANCH
+  #git rev-parse HEAD
+  #exit
   if [ "$CURRENT_BRANCH" = "HEAD" ]; then
-    MODULE=$(pwd | sed 's/.*\///')
-    if [ "$MODULE" = "acquia_cms_common" ]; then
-      git checkout -b develop-1.4.x
-    elif [ "$MODULE" = "acquia_cms_tour" ]; then
-        git checkout -b develop-2.0.x
-    else
-      git checkout -b develop
-    fi
-    CURRENT_BRANCH=$(git rev-parse --abbrev-ref HEAD)
+#    MODULE=$(pwd | sed 's/.*\///')
+#    if [ "$MODULE" = "acquia_cms_common" ]; then
+#      git checkout -b develop-1.4.x
+#    elif [ "$MODULE" = "acquia_cms_tour" ]; then
+#        git checkout -b develop-2.0.x
+#    else
+#      git checkout -b develop
+#    fi
+#    CURRENT_BRANCH=$(git rev-parse --abbrev-ref HEAD)
+    CURRENT_BRANCH=$(git rev-parse HEAD)
+  fi
+#  echo "$NEW_TAG, dev-$CURRENT_BRANCH"
+#  ALIAS="{\"dev-$CURRENT_BRANCH\": \"$NEW_TAG-dev\"}"
+#  composer config extra.branch-alias "$ALIAS" --json
+
+  if [[ "$NEW_TAG" =~ (.*beta.*|.*alpha.*|.*rc.*) ]]; then
+    NEW_TAG="$(echo $NEW_TAG | sed 's/\-.*//')"
   fi
   echo "$NEW_TAG, dev-$CURRENT_BRANCH"
   ALIAS="{\"dev-$CURRENT_BRANCH\": \"$NEW_TAG-dev\"}"
