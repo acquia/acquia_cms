c8aee2be33d3d0d64279f8a54680db5156c7451f
Vishal Khode <vishal.khode@acquia.com>
2022-08-11 02:21:06 +0530

    ACMS-1290: Refactor PHPUnit tests.

---
+++ modules/acquia_cms_video/tests/src/Functional/VideoPermissionsTest.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace Drupal\Tests\acquia_cms_document\Functional;
+
+use Drupal\Tests\acquia_cms_common\Functional\MediaPermissionsTestBase;
+
+/**
+ * Tests basic, broad permissions of the user roles included with Acquia CMS.
+ *
+ * @group acquia_cms_video
+ * @group acquia_cms
+ * @group risky
+ */
+class VideoPermissionsTest extends MediaPermissionsTestBase {
+
+  /**
+   * {@inheritdoc}
+   */
+  protected static $modules = [
+    'acquia_cms_video',
+  ];
+
+  /**
+   * {@inheritdoc}
+   */
+  public function getBundle(): string {
+    return "video";
+  }
+
+}
