ef45fc90e5941ae0e56f1ac5ed5dc9cab43566fe
panshulK <panshul.khurana@acquia.com>
2022-09-19 17:21:35 +0530

    ACMS-1442: Removed the function since it has been already written in drush 10.

--- modules/acquia_cms_common/src/Commands/Hooks.php
+++ modules/acquia_cms_common/src/Commands/Hooks.php
@@ -8,7 +8,6 @@ use Drupal\acquia_cms_common\Services\AcmsUtilityService;
 use Drupal\Core\Config\ConfigFactoryInterface;
 use Drupal\Core\Extension\ModuleHandlerInterface;
 use Drush\Commands\DrushCommands;
-use Drush\Drush;
 
 /**
  * Implements Drush command hooks.
@@ -64,53 +63,6 @@ final class Hooks extends DrushCommands {
       unset($result['uuid'], $result['_core']);
     }
     return $result;
-  }
-
-  /**
-   * Validate the result of the pm:enable command.
-   *
-   * For acquia_cms_starter and acquia_cms_demo_pubsec we implemented the
-   * hook_requirements() to check if the other module is enabled or not.
-   * Drush pm:enable command is not running hook_requirements().
-   *
-   * @todo Reference: https://github.com/drush-ops/drush/pull/4337/files
-   * once patch will get applied to drush we can remove this method.
-   *
-   * @hook validate pm:enable
-   */
-  public function validateRequirement(CommandData $commandData) {
-    $modules = $commandData->input()->getArgument('modules');
-    // Run requirements checks on each module.
-    // @see \drupal_check_module()
-    require_once DRUSH_DRUPAL_CORE . '/includes/install.inc';
-    foreach ($modules as $module) {
-      module_load_install($module);
-      $requirements = $this->moduleHandler->invoke($module, 'requirements', ['install']);
-      if (is_array($requirements) && drupal_requirements_severity($requirements) == REQUIREMENT_ERROR) {
-        $reasons = [];
-        // Print any error messages.
-        foreach ($requirements as $id => $requirement) {
-          if (isset($requirement['severity']) && $requirement['severity'] == REQUIREMENT_ERROR) {
-            $message = $requirement['description'];
-            if (isset($requirement['value']) && $requirement['value']) {
-              $message = dt('@requirements_message (Currently using @item version @version)',
-              [
-                '@requirements_message' => $requirement['description'],
-                '@item' => $requirement['title'],
-                '@version' => $requirement['value'],
-              ]);
-            }
-            $reasons[$id] = "$module: " . (string) $message;
-          }
-        }
-        throw new \Exception(implode("\n", $reasons));
-      }
-    }
-    if (is_array($requirements) && drupal_requirements_severity($requirements) == REQUIREMENT_WARNING) {
-      foreach ($requirements as $id => $requirement) {
-        Drush::logger()->warning(dt($requirement['description']));
-      }
-    }
   }
 
   /**
