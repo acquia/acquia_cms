d027e0968df2c54460e3fca1d433a254c98e4c61
“Chandan <chandan.singh@acquia.com>
2023-01-16 14:08:03 +0530

    ACMS-1493: PHPUnit testcases for API Dashboard - API keys.

---
+++ modules/acquia_cms_headless/tests/src/FunctionalJavascript/HeadlessDashboardTestBase.php
@@ -0,0 +1,74 @@
+<?php
+
+namespace Drupal\Tests\acquia_cms_headless\FunctionalJavascript;
+
+use Acquia\DrupalEnvironmentDetector\AcquiaDrupalEnvironmentDetector;
+use Drupal\FunctionalJavascriptTests\WebDriverTestBase;
+
+/**
+ * Provides Base class to test headless dashboard.
+ *
+ * @group acquia_cms_headless
+ * @group medium_risk
+ * @group push
+ */
+abstract class HeadlessDashboardTestBase extends WebDriverTestBase {
+
+  /**
+   * {@inheritdoc}
+   */
+  protected $defaultTheme = 'stark';
+
+  /**
+   * {@inheritdoc}
+   */
+  protected static $modules = [
+    'acquia_cms_headless',
+  ];
+
+  /**
+   * Disable strict config schema checks in this test.
+   *
+   * Cohesion has a lot of config schema errors, and until they are all fixed,
+   * this test cannot pass unless we disable strict config schema checking
+   * altogether. Since strict config schema isn't critically important in
+   * testing this functionality, it's okay to disable it for now, but it should
+   * be re-enabled (i.e., this property should be removed) as soon as possible.
+   *
+   * @var bool
+   */
+  // @codingStandardsIgnoreStart
+  protected $strictConfigSchema = FALSE;
+  // @codingStandardsIgnoreEnd
+
+  /**
+   * {@inheritdoc}
+   */
+  protected function setUp(): void {
+    // @todo Remove this check when Acquia Cloud IDEs support running functional
+    // JavaScript tests.
+    if (AcquiaDrupalEnvironmentDetector::isAhIdeEnv()) {
+      $this->markTestSkipped('This test cannot run in an Acquia Cloud IDE.');
+    }
+    parent::setUp();
+  }
+
+  /**
+   * Tests that dashboard page is accessible.
+   */
+  public function testDashboard() {
+    $account = $this->drupalCreateUser();
+    $account->addRole('administrator');
+    $account->save();
+    $this->drupalLogin($account);
+
+    $this->drupalGet("/admin/headless/dashboard");
+    $this->assertSession()->statusCodeEquals(200);
+  }
+
+  /**
+   * Test if given section is available.
+   */
+  abstract protected function testSectionAvailable(): void;
+
+}
