8ef59ee49fa236b2ab43576c19f90a7404e61053
Deepak Mishra <deepak.mishra@acquia.com>
2023-01-24 22:16:02 +0530

    ACMS-1490: PHPUnit testcases for API Dashboard - API Endpoints

---
+++ modules/acquia_cms_headless/tests/src/Functional/DashboardApiUrlTest.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace Drupal\Tests\acquia_cms_headless\Functional;
+
+/**
+ * Tests headless dashboard API Url.
+ *
+ * @group acquia_cms
+ * @group acquia_cms_headless
+ * @group medium_risk
+ * @group push
+ */
+class DashboardApiUrlTest extends DashboardWebDriverTestBase {
+
+  /**
+   * {@inheritdoc}
+   */
+  protected string $sectionTitle = "API URL";
+
+  /**
+   * {@inheritdoc}
+   */
+  protected string $sectionSelector = "#acquia-cms-headless-api-url";
+
+  /**
+   * {@inheritdoc}
+   */
+  public function testButtons(): void {
+    $this->assertButton("Update Base API URL");
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function testSection(): void {
+    $element = $this->getSection()->find("css", ".headless-dashboard-api-url");
+    $this->assertSame($element->getText(), 'Base API Url: ' . $this->baseUrl . "/jsonapi");
+  }
+
+}
