ed382a114b64e78a1d98e3a0a3d03ccb8bc0220b
Deepak Mishra <deepak.mishra@acquia.com>
2023-02-03 13:13:14 +0530

    ACMS-1480: created headless browser test base.

---
+++ modules/acquia_cms_headless/tests/src/Functional/HeadlessBrowserTestBase.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace Drupal\Tests\acquia_cms_headless\Functional;
+
+use Acquia\DrupalEnvironmentDetector\AcquiaDrupalEnvironmentDetector;
+use Drupal\Tests\BrowserTestBase;
+
+/**
+ * Base class for the HeadlessDashboard Browser tests.
+ */
+class HeadlessBrowserTestBase extends BrowserTestBase {
+
+  /**
+   * {@inheritdoc}
+   */
+  protected $defaultTheme = 'stark';
+
+  /**
+   * {@inheritdoc}
+   */
+  protected static $modules = [
+    'acquia_cms_headless',
+  ];
+
+  /**
+   * {@inheritdoc}
+   */
+  protected function setUp(): void {
+    // @todo Remove this check when Acquia Cloud IDEs support running functional
+    // JavaScript tests.
+    if (AcquiaDrupalEnvironmentDetector::isAhIdeEnv()) {
+      $this->markTestSkipped('This test cannot run in an Acquia Cloud IDE.');
+    }
+    parent::setUp();
+  }
+
+}
