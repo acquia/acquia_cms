782f3a9f0f6f81020479c792ad168823976311e4
“Chandan <chandan.singh@acquia.com>
2023-02-09 14:44:57 +0530

    ACMS-1994: acquia_cms_headless: PHPUnit testcases for Hybrid Mode.

--- modules/acquia_cms_headless/tests/src/Functional/HeadlessHybridModeTest.php
+++ modules/acquia_cms_headless/tests/src/Functional/HeadlessHybridModeTest.php
@@ -25,31 +25,38 @@ class HeadlessHybridModeTest extends HeadlessTestBase {
     $account->addRole('administrator');
     $account->save();
     $this->drupalLogin($account);
-    $assert_session = $this->assertSession();
-    $session = $this->getSession();
-    $page = $session->getPage();
+    $assertSession = $this->assertSession();
 
-    // Get Fieldset to fetch Headless Dashboard link.
-    $menuItem = $assert_session->elementExists('css', '.toolbar-icon-acquia-cms-tour-tour')->getParent()->mouseOver();
-    $headlessDashboard = $menuItem->getParent()->find('css', '.toolbar-icon-acquia-cms-headless-dashboard');
+    $acmsWizardLi = $assertSession->elementExists('css', '.toolbar-icon-acquia-cms-tour-tour')->getParent();
+    $this->assertNotEmpty($acmsWizardLi);
+    $acmsWizardLi->mouseOver();
+    $headlessDashboard = $assertSession->waitForElementVisible('css', '.toolbar-icon-acquia-cms-headless-dashboard');
     $this->assertEquals('Headless dashboard', $headlessDashboard->getText());
-    $assert_session->statusCodeEquals(200);
 
-    // Test if Enable headless mode checkbox works.
+    // Click on Setup manually button to close the modal,
+    // note there are two button with same label 'Setup manually'.
     $this->drupalGet('/admin/tour/dashboard');
-    $checkbox = $assert_session->elementExists('css', '#edit-headless-mode');
-    $this->assertNotEmpty($checkbox);
-    $checkbox->check();
-    $page->pressButton('Save');
+    $acmsWelcomeModal = $assertSession->waitForElementVisible('css', '.acms-welcome-modal');
+    $btn_panes = $assertSession->elementExists('css', '.ui-dialog-buttonpane', $acmsWelcomeModal);
+    $assertSession->buttonExists('Setup Manually', $btn_panes)->press();
+
+    // Test Enable headless mode checkbox functionality.
+    $headlessForm = $assertSession->elementExists('css', '#acquia-cms-headless-form');
+    $assertSession->elementExists('css', '.claro-details__summary', $headlessForm)->press();
+    $headlessForm->checkField('Enable Headless mode');
+    $headlessForm->pressButton('Save');
+
+    // Ensure that after save button headless mode is enabled.
+    $assertSession->pageTextContains('Acquia CMS Pure Headless has been enabled.');
 
     // Test Headless Dashboard link is not available.
-    $toolbar = $assert_session->elementExists('css', '#toolbar-administration');
-    $tourLink = $assert_session->elementExists('named', ['link', 'Acquia CMS Wizard'], $toolbar);
+    $toolbar = $assertSession->elementExists('css', '#toolbar-administration');
+    $tourLink = $assertSession->elementExists('named', ['link', 'Acquia CMS Wizard'], $toolbar);
     $this->assertSame('Acquia CMS Wizard', $tourLink->getText());
     $this->assertTrue($tourLink->hasClass('toolbar-icon'));
 
     // Headless dashboard should not show up.
-    $assert_session->elementNotExists('named', ['link', 'Headless dashboard'], $toolbar);
+    $assertSession->elementNotExists('named', ['link', 'Headless dashboard'], $toolbar);
   }
 
 }
