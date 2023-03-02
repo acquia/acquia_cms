4294e90b41ed6717e7f8103177d385f4ce19db30
“Chandan <chandan.singh@acquia.com>
2023-03-02 17:54:51 +0530

    ACMS-1614: Update script file permisions.

--- tests/ci/blt_si.sh
+++ tests/ci/blt_si.sh
@@ -1,8 +1,25 @@
 #!/usr/bin/env bash
 
+cd "$(dirname "$0")"
+
+# Reuse ORCA's own includes.
+source ../../../orca/bin/ci/_includes.sh
+
+# If there is no fixture, there's nothing else for us to do.
+[[ -d "${ORCA_FIXTURE_DIR}" ]] || exit 0
+
+cd ${ORCA_FIXTURE_DIR}
+
 ./vendor/bin/drush config:set system.site uuid ${UUID}
 ./vendor/bin/drush cex --yes
 ./vendor/bin/drush site:install minimal --yes --uri=http://127.0.0.1:8080
 ./vendor/bin/drush config:set system.site uuid ${UUID} --yes
 ./vendor/bin/drush cim --yes
-./vendor/bin/drush config:status
+./vendor/bin/drush cr
+./vendor/bin/drush cim --yes
+./vendor/bin/drush cex --yes
+
+CONFIG_STATUS=$(./vendor/bin/drush config:status 2>&1 | grep "No differences")
+if [ -z "${CONFIG_STATUS}" ]; then
+  exit 1
+fi
