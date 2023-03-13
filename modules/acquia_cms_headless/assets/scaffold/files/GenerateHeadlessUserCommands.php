bde966562846072f000cffb95fb75201aed499a0
Ankit Pathak <ankit.pathak@acquia.com>
2023-03-13 14:28:25 +0530

    ACMS-1659: Adding return type for getting proper result of the command.

--- modules/acquia_cms_headless/assets/scaffold/files/GenerateHeadlessUserCommands.php
+++ modules/acquia_cms_headless/assets/scaffold/files/GenerateHeadlessUserCommands.php
@@ -3,6 +3,7 @@
 namespace Drush\Commands;
 
 use Consolidation\AnnotatedCommand\CommandData;
+use Consolidation\AnnotatedCommand\CommandResult;
 
 /**
  * Programmatically create a new "headless" user on config import.
@@ -14,13 +15,20 @@ class GenerateHeadlessUserCommands extends DrushCommands {
    *
    * @hook post-command config-import
    */
-  public function postImportCreateHeadlessUser($result, CommandData $commandData): void {
-    $acmsUtilityService = \Drupal::getContainer()->get('acquia_cms_common.utility');
-    $starterKitNextjsService = \Drupal::getContainer()->get('acquia_cms_headless.starterkit_nextjs');
-    // Programmatically create a new "headless" user.
-    if ($acmsUtilityService->getModulePreinstallTriggered() === 'acquia_cms_headless') {
+  public function postImportCreateHeadlessUser($result, CommandData $commandData): CommandResult {
+    // Check headless is installed.
+    if (\Drupal::state()->get('acquia_cms_headless.status') === 'enabled') {
+      $starterKitNextjsService = \Drupal::getContainer()->get('acquia_cms_headless.starterkit_nextjs');
+      // Programmatically create a new "headless" user.
+      $this->yell("Creating headless user.");
       $starterKitNextjsService->createHeadlessUser();
+      // Delete after user creation.
+      \Drupal::state()->delete('acquia_cms_headless.status');
+
+      return is_array($result) && isset(array_shift($result)['error']) ? CommandResult::exitCode(self::EXIT_FAILURE) : CommandResult::exitCode(self::EXIT_SUCCESS);
     }
+
+    return is_array($result) && isset(array_shift($result)['error']) ? CommandResult::exitCode(self::EXIT_FAILURE) : CommandResult::exitCode(self::EXIT_SUCCESS);
   }
 
 }
