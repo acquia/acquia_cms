796753991a55f98917af3f4a91652ffda59f9654
Ankit Pathak <ankit.pathak@acquia.com>
2023-03-23 10:06:52 +0530

    ACMS-1483: Added parameter for env file to headless drush command test.

--- modules/acquia_cms_headless/tests/src/Functional/HeadlessDrushCommandsTest.php
+++ modules/acquia_cms_headless/tests/src/Functional/HeadlessDrushCommandsTest.php
@@ -33,12 +33,14 @@ class HeadlessDrushCommandsTest extends BrowserTestBase {
    * {@inheritdoc}
    */
   public function testHeadlessCommands(): void {
-    $options = [
-      'site-url' => 'http://localhost:3000',
-      'site-name' => 'Headless site',
-    ];
+    // Validate required parametes on false positive.
+    $this->drush('acms:headless:new-nextjs', [], ['site-url' => 'http://localhost:3000'], NULL, NULL, 1);
+    $this->assertEquals('[error]  Missing required parameter site name.', $this->getErrorOutput());
+    $this->drush('acms:headless:new-nextjs', [], ['site-name' => 'Headless site'], NULL, NULL, 1);
+    $this->assertEquals('[error]  Missing required parameter site URL.', $this->getErrorOutput());
+
     // Execute new headless site.
-    $this->drush('acms:headless:new-nextjs', [], $options);
+    $this->drush('acms:headless:new-nextjs', [], ['site-url' => 'http://localhost:3000', 'site-name' => 'Headless site']);
     $newNextJsData = $this->getErrorOutputAsList();
     $this->assertEquals("Use these environment variables for your Next.js application. Place them in your .env file:", str_replace("[notice] ", "", $newNextJsData[0]));
     $this->assertEquals("NEXT_PUBLIC_DRUPAL_BASE_URL=http://default", $newNextJsData[1]);
@@ -48,6 +50,14 @@ class HeadlessDrushCommandsTest extends BrowserTestBase {
     $this->assertStringStartsWith("DRUPAL_PREVIEW_SECRET", $newNextJsData[5]);
     $this->assertStringStartsWith("DRUPAL_CLIENT_ID", $newNextJsData[6]);
     $this->assertStringStartsWith("DRUPAL_CLIENT_SECRET", $newNextJsData[7]);
+
+    // Validate for same site-name.
+    $this->drush('acms:headless:new-nextjs', [], ['site-url' => 'http://localhost:3000', 'site-name' => 'Headless site', 'env-file' => '../frontend/.env.local'], NULL, NULL, 1);
+    $this->assertEquals('[error]  Site with name [Headless site] already exists!', $this->getErrorOutput());
+
+    // Execute drush new nextjs site with env file parameter.
+    $this->drush('acms:headless:new-nextjs', [], ['site-url' => 'http://localhost:3001', 'site-name' => 'Headless next site', 'env-file' => '../frontend/.env.local']);
+    $this->assertEquals('[success] Environment variables were written to ../frontend/.env.local', $this->getErrorOutput());
 
     // Execute headless regenerate environment.
     $this->drush('acms:headless:regenerate-env', [], ['site-url' => 'http://localhost:3000']);
@@ -59,6 +69,11 @@ class HeadlessDrushCommandsTest extends BrowserTestBase {
     }
     // Client secret will be change while regenerating environment.
     $this->assertNotEquals($newNextJsData[7], $clientSecret);
+
+    // Execute drush regenerate environment
+    // for nextjs site with env file parameter.
+    $this->drush('acms:headless:regenerate-env', [], ['site-url' => 'http://localhost:3001', 'env-file' => '../frontend/.env.local']);
+    $this->assertEquals('[success] Environment variables were written to ../frontend/.env.local', $this->getErrorOutput());
   }
 
 }
