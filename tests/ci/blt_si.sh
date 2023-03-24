3322fbe407fdbfacf496e60dd23f837d54bf8041
“Chandan <chandan.singh@acquia.com>
2023-03-24 14:53:01 +0530

    ACMS-1705: Update tests accordingly.

--- tests/ci/blt_si.sh
+++ tests/ci/blt_si.sh
@@ -10,13 +10,11 @@ source ../../../orca/bin/ci/_includes.sh
 
 cd ${ORCA_FIXTURE_DIR}
 
-./vendor/bin/drush en acquia_cms_site_studio_cm -y
+./vendor/bin/drush en acquia_cms_site_studio_cm --yes
 ./vendor/bin/drush config:set system.site uuid ${UUID}
 ./vendor/bin/drush cex --yes
 ./vendor/bin/drush site:install minimal --yes --uri=http://127.0.0.1:8080
 ./vendor/bin/drush config:set system.site uuid ${UUID} --yes
-./vendor/bin/drush cim --yes
-./vendor/bin/drush cr
 ./vendor/bin/drush cim --yes
 ./vendor/bin/drush cex --yes
 
