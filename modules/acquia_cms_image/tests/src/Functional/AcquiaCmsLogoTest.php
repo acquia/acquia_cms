83101c8ccb553cbea7a111e49bf7c8d30cc96f4b
Rajeshreeputra <pravin.gaikwad@acquia.com>
2023-04-21 18:43:59 +0530

    ACMS-1663: review feedback updates.

--- modules/acquia_cms_image/tests/src/Functional/AcquiaCmsLogoTest.php
+++ modules/acquia_cms_image/tests/src/Functional/AcquiaCmsLogoTest.php
@@ -1,12 +1,12 @@
 <?php
 
-namespace Drupal\acquia_cms_image\ExistingSite;
+namespace Drupal\acquia_cms_image\Functional;
 
 use Drupal\media\Entity\Media;
 use Drupal\Tests\BrowserTestBase;
 
 /**
- * Test if Acquia CMS logo exist or not.
+ * Test that Acquia CMS logo exists and has correct image file.
  */
 class AcquiaCmsLogoTest extends BrowserTestBase {
 
@@ -43,15 +43,17 @@ class AcquiaCmsLogoTest extends BrowserTestBase {
     $this->assertSession()->statusCodeEquals(200);
 
     // Load media and verify the uuid is same.
-    $media = Media::load(1);
+    $media = $this->container->get('entity_type.manager')->getStorage('media')->load(1);
     $this->assertInstanceOf(Media::class, $media, "Media `{1}` should exist.");
     $this->assertEquals($media->get('uuid')->getValue()[0]['value'], '0c6f0f26-9fbb-4c2e-804c-418815aba162');
 
     // Load node page and verify logo path.
-    $this->drupalGet('/node');
+    $this->drupalGet('<front>');
     $this->assertSession()->statusCodeEquals(200);
     $site_path = $this->container->getParameter('site.path');
-    $this->assertEquals($this->assertSession()->elementExists('css', '#block-olivero-site-branding > div.site-branding__inner > a > img')->getAttribute('src'), "/$site_path/files/media-icons/Acquia-CMS.png");
+    $this->assertEquals($this->assertSession()
+      ->elementExists('css', '#block-olivero-site-branding > div.site-branding__inner > a > img')
+      ->getAttribute('src'), "/$site_path/files/media-icons/acquia_cms_logo.png");
   }
 
 }
