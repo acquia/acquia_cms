b7b03ac58663fcbb6d616ff516662f4745f6edd0
Rajeshreeputra <pravin.gaikwad@acquia.com>
2023-05-05 12:26:37 +0530

    ACMS-1730: add event subscriber to create user post config import.

---
+++ modules/acquia_cms_headless/src/EventSubscriber/HeadlessPostConfigEventsSubscriber.php
@@ -0,0 +1,53 @@
+<?php
+
+namespace Drupal\acquia_cms_headless\EventSubscriber;
+
+use Drupal\acquia_cms_headless\Service\StarterkitNextjsService;
+use Drupal\acquia_config_management\Event\ConfigEvent;
+use Symfony\Component\EventDispatcher\EventSubscriberInterface;
+
+/**
+ * Process headless related manipulation on post configuration import/export.
+ *
+ * @package Drupal\acquia_cms_headless\EventSubscriber
+ */
+class HeadlessPostConfigEventsSubscriber implements EventSubscriberInterface {
+
+  /**
+   * Starter-kit service.
+   *
+   * @var \Drupal\acquia_cms_headless\Service\StarterkitNextjsService
+   */
+  protected $starterKitService;
+
+  /**
+   * Constructs a new HeadlessPostConfigEventsSubscriber object.
+   *
+   * @param \Drupal\acquia_cms_headless\Service\StarterkitNextjsService $acms_starter_kit_service
+   *   The acms utility service.
+   */
+  public function __construct(StarterkitNextjsService $acms_starter_kit_service) {
+    $this->starterKitService = $acms_starter_kit_service;
+  }
+
+  /**
+   * {@inheritdoc}
+   *
+   * @return array
+   *   The event names to listen for, and the methods that should be executed.
+   */
+  public static function getSubscribedEvents() {
+    return [
+      ConfigEvent::POST_CONFIG_IMPORT => 'onPostConfigImport',
+    ];
+  }
+
+  /**
+   * Post config import manipulation.
+   */
+  public function onPostConfigImport() {
+    // Create headless user.
+    $this->starterKitService->createHeadlessUser();
+  }
+
+}
