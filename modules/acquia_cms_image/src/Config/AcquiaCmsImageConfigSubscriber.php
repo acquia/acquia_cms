f32db148ac552fe29edba749c8a9bb53721a98e0
Vishal Khode <vishal.khode@acquia.com>
2023-05-15 20:03:59 +0530

    ACMS-1711: Refactored logic to create/add site logo.

--- modules/acquia_cms_image/src/Config/AcquiaCmsImageConfigSubscriber.php
+++ modules/acquia_cms_image/src/Config/AcquiaCmsImageConfigSubscriber.php
@@ -2,8 +2,10 @@
 
 namespace Drupal\acquia_cms_image\Config;
 
+use Drupal\acquia_cms_image\SiteLogo;
 use Drupal\Core\Config\ConfigEvents;
 use Drupal\Core\Config\ConfigImporterEvent;
+use Drupal\Core\DependencyInjection\ClassResolverInterface;
 use Symfony\Component\EventDispatcher\EventSubscriberInterface;
 
 /**
@@ -12,13 +14,17 @@ use Symfony\Component\EventDispatcher\EventSubscriberInterface;
 class AcquiaCmsImageConfigSubscriber implements EventSubscriberInterface {
 
   /**
-   * Creates default content after config synchronization.
+   * The SiteLogo class object.
    *
-   * @param \Drupal\Core\Config\ConfigImporterEvent $event
-   *   The config importer event.
+   * @var \Drupal\acquia_cms_image\SiteLogo
    */
-  public function onConfigImport(ConfigImporterEvent $event) {
-    _acquia_cms_image_import_logo();
+  protected $siteLogo;
+
+  /**
+   * {@inheritdoc}
+   */
+  public function __construct(ClassResolverInterface $classResolver) {
+    $this->siteLogo = $classResolver->getInstanceFromDefinition(SiteLogo::class);
   }
 
   /**
@@ -26,6 +32,27 @@ class AcquiaCmsImageConfigSubscriber implements EventSubscriberInterface {
    */
   public static function getSubscribedEvents() {
     return [ConfigEvents::IMPORT => 'onConfigImport'];
+  }
+
+  /**
+   * Creates default content after config synchronization.
+   *
+   * @param \Drupal\Core\Config\ConfigImporterEvent $event
+   *   The config importer event.
+   *
+   * @throws \Drupal\Core\Entity\EntityStorageException
+   */
+  public function onConfigImport(ConfigImporterEvent $event) {
+    $enabled_extensions = $event->getConfigImporter()->getExtensionChangelist('module', 'install');
+    $themeConfig = $event->getConfigImporter()->getStorageComparer()->getSourceStorage()->read("system.theme.global");
+    $logoPath = $themeConfig['logo']['path'] ?? '';
+
+    // We are creating media content for logo, if module is getting installed
+    // using existing configurations and if the user has not changed the logo
+    // provided by the acquia_cms_image module.
+    if (in_array('acquia_cms_image', $enabled_extensions) && $logoPath == SiteLogo::LOGO_PATH) {
+      $this->siteLogo->createLogo();
+    }
   }
 
 }
