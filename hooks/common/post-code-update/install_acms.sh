0aa5b2335161cb1b76969af3f8f35ca3d4539391
Vishal Khode <vishal.khode@acquia.com>
2023-07-17 22:04:25 +0530

    ACMS-1903: Fix the failing site installtion issue on Acquia Cloud.

--- hooks/common/post-code-update/install_acms.sh
+++ hooks/common/post-code-update/install_acms.sh
@@ -1,40 +1,55 @@
-#!/bin/sh
-# Cloud Hook: Acquia CMS install,update and modules toggle.
+#!/usr/bin/env bash
+#
+# Factory Hook: db-update
+#
+# Run cohesion commands after executing
+
+SITEGROUP="$1"
+ENVIRONMENT="$2"
+DB_ROLE="$3"
+DOMAIN="$4"
+ATTEMPT="$5"
+
+echo "Sitegroup: $SITEGROUP"
+echo "Environment: $ENVIRONMENT"
+echo "DB role: $DB_ROLE"
+echo "Domain: $DOMAIN"
+echo "Attempt: $ATTEMPT"
+
+# Drush executable:
+drush="/mnt/www/html/$SITEGROUP.$ENVIRONMENT/vendor/bin/drush"
 
 which drush
 drush --version
 
-site="$1"
-target_env="$2"
-
 # Fresh install of Acquia CMS. We need to clear cache first in case memcache is
 # enabled, else there will be a collision on site install.
-/usr/local/bin/drush9 @$site.$target_env cr
+drush @SITEGROUP.$ENVIRONMENT cr
 
 # Only run update hooks on ode4. ode4 is used to test update path.
-if [ "$target_env" = "ode5" ]; then
-    /usr/local/bin/drush9 @$site.$target_env updatedb --no-interaction
+if [ "$ENVIRONMENT" = "ode5" ]; then
+    drush @SITEGROUP.$ENVIRONMENT updatedb --no-interaction
 # Install Acquia CMS.
 else
-    /var/www/html/$site.$target_env/vendor/bin/acms site-install minimal --account-pass=admin --yes --account-mail=no-reply@example.com --site-mail=no-reply@example.com
+    /var/www/html/$SITEGROUP.$ENVIRONMENT/vendor/bin/acms site-install minimal --account-pass=admin --yes --account-mail=no-reply@example.com --site-mail=no-reply@example.com
 fi
 
 # Toggle Modules based on the environment.
-/usr/local/bin/drush9 @$site.$target_env pm-enable acquia_cms_development --yes
+drush @SITEGROUP.$ENVIRONMENT pm-enable acquia_cms_development --yes
 
 # Enable development related modules. This is for ease of development for core
 # Acquia CMS development.
-echo "Enabling Acquia CMS Starter in $target_env"
-case $target_env in
+echo "Enabling Acquia CMS Starter in $ENVIRONMENT"
+case $ENVIRONMENT in
   ode1)
-    /usr/local/bin/drush9 @$site.$target_env pm-enable acquia_cms_starter --yes
+    drush @SITEGROUP.$ENVIRONMENT pm-enable acquia_cms_starter --yes
     ;;
 
   ode3)
-    /usr/local/bin/drush9 @$site.$target_env pm-enable acquia_cms_starter --yes
+    drush @SITEGROUP.$ENVIRONMENT pm-enable acquia_cms_starter --yes
     ;;
 
   stage)
-    /usr/local/bin/drush9 @$site.$target_env pm-enable acquia_cms_starter --yes
+    drush @SITEGROUP.$ENVIRONMENT pm-enable acquia_cms_starter --yes
     ;;
 esac
