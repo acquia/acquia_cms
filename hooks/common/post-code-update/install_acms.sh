4ec46a5e764f06f4ebab04cebf8bd2395eaf118f
Vishal Khode <vishal.khode@acquia.com>
2023-07-17 22:11:15 +0530

    ACMS-1903: Update acquia/acquia-cms-starterkit to the latest.

--- hooks/common/post-code-update/install_acms.sh
+++ hooks/common/post-code-update/install_acms.sh
@@ -24,32 +24,32 @@ drush --version
 
 # Fresh install of Acquia CMS. We need to clear cache first in case memcache is
 # enabled, else there will be a collision on site install.
-drush @SITEGROUP.$ENVIRONMENT cr
+drush @$SITEGROUP.$ENVIRONMENT cr
 
 # Only run update hooks on ode4. ode4 is used to test update path.
 if [ "$ENVIRONMENT" = "ode5" ]; then
-    drush @SITEGROUP.$ENVIRONMENT updatedb --no-interaction
+    drush @$SITEGROUP.$ENVIRONMENT updatedb --no-interaction
 # Install Acquia CMS.
 else
     /var/www/html/$SITEGROUP.$ENVIRONMENT/vendor/bin/acms site-install minimal --account-pass=admin --yes --account-mail=no-reply@example.com --site-mail=no-reply@example.com
 fi
 
 # Toggle Modules based on the environment.
-drush @SITEGROUP.$ENVIRONMENT pm-enable acquia_cms_development --yes
+drush @$SITEGROUP.$ENVIRONMENT pm-enable acquia_cms_development --yes
 
 # Enable development related modules. This is for ease of development for core
 # Acquia CMS development.
 echo "Enabling Acquia CMS Starter in $ENVIRONMENT"
 case $ENVIRONMENT in
   ode1)
-    drush @SITEGROUP.$ENVIRONMENT pm-enable acquia_cms_starter --yes
+    drush @$SITEGROUP.$ENVIRONMENT pm-enable acquia_cms_starter --yes
     ;;
 
   ode3)
-    drush @SITEGROUP.$ENVIRONMENT pm-enable acquia_cms_starter --yes
+    drush @$SITEGROUP.$ENVIRONMENT pm-enable acquia_cms_starter --yes
     ;;
 
   stage)
-    drush @SITEGROUP.$ENVIRONMENT pm-enable acquia_cms_starter --yes
+    drush @$SITEGROUP.$ENVIRONMENT pm-enable acquia_cms_starter --yes
     ;;
 esac
