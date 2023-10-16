b662ace06a0591ef2b0b70063b0f976e65492990
Rajeshreeputra <pravin.gaikwad@acquia.com>
2023-10-16 16:26:12 +0530

    ACMS-1989: Fix failing text after changing admin theme to gin.

--- tests/src/ExistingSiteJavascript/CohesionTestBase.php
+++ tests/src/ExistingSiteJavascript/CohesionTestBase.php
@@ -158,7 +158,7 @@ abstract class CohesionTestBase extends ExistingSiteSelenium2DriverTestBase {
    */
   protected function pressSaveButton(): void {
     $this->assertSession()
-      ->elementExists('css', '#edit-actions')
+      ->elementExists('css', '#edit-submit')
       ->pressButton('Save');
   }
 
