4ed18c04f08014e9f89b4d25cd6245ecf2fdf81d
“Chandan <chandan.singh@acquia.com>
2024-03-01 18:31:50 +0530

    ACMS-3628: Refactor test for site studio.

--- modules/acquia_cms_site_studio/tests/src/Functional/SiteStudioCoreTest.php
+++ modules/acquia_cms_site_studio/tests/src/Functional/SiteStudioCoreTest.php
@@ -53,25 +53,18 @@ class SiteStudioCoreTest extends BrowserTestBase {
     // Visit the tour page.
     $this->drupalGet('/admin/tour/dashboard');
     $assert_session->statusCodeEquals(200);
-    $container = $assert_session->elementExists('css', '.acquia-cms-site-studio-core-form');
-    // Assert that save and advanced buttons are present on form.
+
+    // Assert that site studio form exists.
+    $assert_session->elementExists('css', '.acquia-cms-site-studio-core-form');
+
+    // Assert that Save & Ignore buttons are present on form.
     $assert_session->buttonExists('Save');
+    $assert_session->buttonExists('Ignore');
+    $assert_session->linkExists('Advanced');
+
     // Assert that the expected fields show up.
     $assert_session->fieldExists('API key');
     $assert_session->fieldExists('Agency key');
-    // Save API key.
-    $dummy_api_key = 'test-key-13fdnj32';
-    $container->fillField('edit-api-key', $dummy_api_key);
-    // Save Agency key.
-    $dummy_agency_key = 'test-agency-13fdnj32';
-    $container->fillField('edit-agency-key', $dummy_agency_key);
-    $container->pressButton('Save');
-    $assert_session->pageTextContains('The configuration options have been saved.');
-    // Test that the config values we expect are set correctly.
-    $cohesion_api_key = $this->config('cohesion.settings')->get('api_key');
-    $this->assertSame($cohesion_api_key, $dummy_api_key);
-    $cohesion_agency_key = $this->config('cohesion.settings')->get('organization_key');
-    $this->assertSame($cohesion_agency_key, $dummy_agency_key);
   }
 
 }
