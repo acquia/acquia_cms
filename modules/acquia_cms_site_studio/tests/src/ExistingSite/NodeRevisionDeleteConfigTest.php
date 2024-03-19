73268d635dc88497d603a778a0c186290ebfde3b
Ankit Pathak <ankit.pathak@acquia.com>
2024-03-19 12:57:53 +0530

    ACMS-3650: Fix existingTest failure for node revision delete with 2.x version.

--- modules/acquia_cms_site_studio/tests/src/ExistingSite/NodeRevisionDeleteConfigTest.php
+++ modules/acquia_cms_site_studio/tests/src/ExistingSite/NodeRevisionDeleteConfigTest.php
@@ -35,7 +35,33 @@ class NodeRevisionDeleteConfigTest extends ExistingSiteBase {
     // Check that node revision delete default configs are in place.
     $config = $this->config('node_revision_delete.settings');
     if ($config) {
+      // Total revisions set for delete.
+      $this->assertTrue($config->get('defaults.amount.status'));
+      $this->assertEquals(50, $config->get('defaults.amount.settings.amount'));
+      // Total duration i.e 12 months is minimum to keep revisions.
+      $this->assertTrue($config->get('defaults.created.status'));
+      $this->assertEquals(12, $config->get('defaults.created.settings.age'));
+      $this->assertTrue($config->get('defaults.drafts.status'));
+      $this->assertEquals(12, $config->get('defaults.drafts.settings.age'));
+      // By default it is set to FALSE.
       $this->assertFalse($config->get('disable_automatic_queueing'));
+    }
+  }
+
+  /**
+   * Assert that node revision delete configs available for Page content type.
+   */
+  public function testContentTypeConfig() {
+    // Check node revision delete configs for Page content type are in palce.
+    $config = $this->config('node.type.page');
+    if ($config) {
+      $this->assertTrue($config->get('third_party_settings.node_revision_delete.amount.status'));
+      // Total revisions for Page content type.
+      $this->assertSame(30, $config->get('third_party_settings.node_revision_delete.amount.settings.amount'));
+      // Keep revisions for active and drafts contents.
+      $this->assertSame(0, $config->get('third_party_settings.node_revision_delete.created.settings.age'));
+      $this->assertSame(0, $config->get('third_party_settings.node_revision_delete.drafts.settings.age'));
+      $this->assertSame(0, $config->get('third_party_settings.node_revision_delete.drafts_only.settings.age'));
     }
   }
 
