264aadf6599aa44d23d4d35b663ea830f341ad70
Vishal Khode <vishal.khode@acquia.com>
2024-09-20 16:44:22 +0530

    Removed sitestudio_config_management from everywhere.

--- tests/ci/before_install.blt.sh
+++ tests/ci/before_install.blt.sh
@@ -24,8 +24,6 @@ do
   composer require "${module_dependency}" --no-update --no-install
 done
 
-composer require "drupal/sitestudio_config_management" --no-update --no-install
-
 # Remove the modules folder. This is very important, otherwise
 # Drupal will see all acquia_cms modules from two folders i.e
 #  1. ./docroot/modules/contrib/acquia_cms/modules/acquia_cms_common
